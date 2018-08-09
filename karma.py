b88f7d844521717a10033a57246d3be075271ba1
Matej Soroka <hi@matejsoroka.com>
2018-08-09 23:58:37 +0200

    inheritance bugfix

--- karma.py
+++ karma.py
@@ -6,14 +6,14 @@ class Karma(BaseModel):
 
 
 	def valid_emoji(self, emojiId):
-		row = BaseModel.get_row("bot_karma_emoji", "emoji_id = {}".format(emojiId))
+		row = self.get_row("bot_karma_emoji", "emoji_id = {}".format(emojiId))
 		return row[1] if row else 0
 
 	def update_karma(self, member, emojiValue):
 		db = sqlite3.connect('database.db') # sqlite connection
 		cursor = db.cursor()
 
-		if self.get_karma(self, member.id):
+		if self.get_karma(member.id):
 			updated = cursor.execute('SELECT karma FROM bot_karma WHERE member_id = ?', [member.id]).fetchone()
 			update = int(updated[0]) + emojiValue
 			cursor.execute('UPDATE bot_karma SET karma = ? WHERE member_id = ?', (update, member.id))
@@ -34,7 +34,7 @@ class Karma(BaseModel):
 			self.update_karma(member, emojiValue*-1)
 
 	def get_karma(self, user_id):
-		row = BaseModel.get_row(self, "bot_karma", "member_id = {}".format(user_id))
+		row = self.get_row("bot_karma", "member_id = {}".format(user_id))
 		return row[1] if row else 0
 
 	def get_leaderboard(self):
