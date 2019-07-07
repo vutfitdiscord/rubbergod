e61341d4f86d2fc74f4cf2892f8f90445e36c9f1
toaster192 <toastercz@gmail.com>
2019-07-08 00:15:33 +0200

    Add missing files

---
+++ repository/reaction.py
@@ -0,0 +1,154 @@
+from repository.base_repository import BaseRepository
+import discord
+
+
+class Reaction(BaseRepository):
+
+    def __init__(self, client, utils, karma):
+        self.client = client
+        self.utils = utils
+        self.karma = karma
+
+    # Returns list of role names and emotes that represent them
+    async def get_join_role_data(self, message):
+        input_string = message.content
+        input_string = input_string.replace(" - ", " ")
+        input_string = input_string.replace(": ", " ")
+        input_string = input_string.replace("**", "")
+        output = []
+        try:
+            input_string = (input_string[input_string.index('\n')+1:]
+                            .strip().split('\n'))
+        except ValueError:
+            await message.channel.send("{} nesprávny formát. Použi !god"
+                                       .format(self.utils.generate_mention(
+                                            message.author.id)))
+            return output
+        for line in input_string:
+            line = line.split()
+            if len(line) > 1:
+                line = [line[0], line[1]]
+                output.append(line)
+            else:
+                await message.channel.send("{} {} je neplatný riadok"
+                                           .format(self.utils.generate_mention(
+                                                message.author.id), line[0]))
+        return output
+
+    # Adds reactions to message
+    async def message_role_reactions(self, message, data):
+        if message.channel.type is not discord.ChannelType.text:
+            await message.channel.send("Nepises na serveru takze predpokladam "
+                                       "ze myslis role VUT FIT serveru")
+            guild = self.client.get_guild(self.config.guild_id)
+        else:
+            guild = message.guild
+        for line in data:
+            if (discord.utils.get(guild.roles,
+                                  name=line[0]) is None):
+                await message.channel.send("{} {} nie je rola"
+                                           .format(self.utils.generate_mention(
+                                               message.author.id), line[0]))
+            else:
+                try:
+                    await message.add_reaction(line[1])
+                except discord.errors.HTTPException:
+                    await message.channel.send(
+                            "{} {} pre rolu {} nie je emote"
+                            .format(self.utils.generate_mention(
+                                        message.author.id),
+                                    line[1], line[0]))
+
+    async def Add(self, payload):
+        channel = self.client.get_channel(payload.channel_id)
+        if channel is None:
+            return
+        if channel.type is discord.ChannelType.text:
+            guild = channel.guild
+        else:
+            guild = self.client.get_guild(self.config.guild_id)
+        member = guild.get_member(payload.user_id)
+        message = await channel.fetch_message(payload.message_id)
+        if member is not None:
+            if payload.emoji.is_custom_emoji():
+                emoji = self.client.get_emoji(payload.emoji.id)
+            else:
+                emoji = payload.emoji.name
+            if not(member.bot):
+                if message.content.startswith(self.config.role_string):
+                    role_data = await self.get_join_role_data(message)
+                    for line in role_data:
+                        if str(emoji) == line[1]:
+                            await self.add_role_on_reaction(line[0], member,
+                                                            message.channel,
+                                                            guild)
+                            break
+                    else:
+                        if emoji is None:
+                            await message.remove_reaction(payload.emoji,
+                                                          member)
+                        else:
+                            await message.remove_reaction(emoji, member)
+                elif message.content.startswith(self.config.vote_message):
+                    if emoji is None:
+                        await message.remove_reaction(payload.emoji, member)
+                    elif emoji not in ["✅", "❌", "0⃣"]:
+                        await message.remove_reaction(emoji, member)
+                elif type(emoji) is not str and member.id != message.author.id:
+                    self.karma.karma_emoji(message.author, payload.emoji.id)
+
+    async def Remove(self, payload):
+        channel = self.client.get_channel(payload.channel_id)
+        if channel is None:
+            return
+        if channel.type is discord.ChannelType.text:
+            guild = channel.guild
+        else:
+            guild = self.client.get_guild(self.config.guild_id)
+        member = guild.get_member(payload.user_id)
+        message = await channel.fetch_message(payload.message_id)
+        if member is not None:
+            if payload.emoji.is_custom_emoji():
+                emoji = self.client.get_emoji(payload.emoji.id)
+            else:
+                emoji = payload.emoji.name
+            if message.content.startswith(self.config.role_string):
+                role_data = await self.get_join_role_data(message)
+                for line in role_data:
+                    if str(emoji) == line[1]:
+                        await self.remove_role_on_reaction(line[0], member,
+                                                           message.channel,
+                                                           guild)
+                        break
+            elif type(emoji) is not str and member.id != message.author.id:
+                self.karma.karma_emoji_remove(message.author, payload.emoji.id)
+
+    # Adds a role for user based on reaction
+    async def add_role_on_reaction(self, role, member, channel, guild):
+        role = discord.utils.get(guild.roles,
+                                 name=role)
+        max_role = discord.utils.get(guild.roles,
+                                     name="Rubbergod")
+        if role is not None:
+            if role < max_role:
+                await member.add_roles(role)
+            else:
+                await channel.send("{} na pridanie role {} nemáš práva"
+                                   .format(self.utils.generate_mention(
+                                           member.id), role.name))
+
+    # Removes a role for user based on reaction
+    async def remove_role_on_reaction(self, role, member, channel, guild):
+        role = discord.utils.get(guild.roles,
+                                 name=role)
+        max_role = discord.utils.get(guild.roles,
+                                     name="Rubbergod")
+        if role is not None:
+            if role in member.roles:
+                if role < max_role:
+                    await member.remove_roles(role)
+                else:
+                    await channel.send("{} na odobranie role {} "
+                                       "nemáš práva"
+                                       .format(self.utils.generate_mention(
+                                               member.id), role.name))
