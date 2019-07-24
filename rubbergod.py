e5b670e9acbbc23c137e31185e32896f148530ab
toaster192 <toastercz@gmail.com>
2019-07-24 18:50:14 +0200

    Add uh oh

--- rubbergod.py
+++ rubbergod.py
@@ -88,6 +88,8 @@ async def on_message(message):
     if message.content.startswith(config.role_string):
         role_data = await reaction.get_join_role_data(message)
         await reaction.message_role_reactions(message, role_data)
+    elif message.content == "uh oh":
+        await message.channel.send("uh oh")
     else:
         await bot.process_commands(message)
 
