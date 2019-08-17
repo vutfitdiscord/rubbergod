613ffc837829e8fd714923d406c98ae818c65851
toaster192 <toastercz@gmail.com>
2019-08-17 12:45:27 +0200

    Add an ugly mysqldump import function

--- rubbergod.py
+++ rubbergod.py
@@ -1,5 +1,6 @@
 import datetime
 import traceback
+import re
 from random import choice
 
 import discord
@@ -10,6 +11,92 @@ from config import config, messages
 from features import (karma, presence, verification, reaction)
 from logic import roll_dice, rng
 from repository import (karma_repo, user_repo)
+
+from repository.database import database, session
+from repository.database.karma import Karma, Karma_emoji
+from repository.database.verification import Permit, Valid_person
+
+
+# TODO move this ANYWHERE else
+def load_dump():
+    database.base.metadata.create_all(database.db)
+
+    session.query(Karma).delete()
+    session.query(Karma_emoji).delete()
+    session.query(Permit).delete()
+    session.query(Valid_person).delete()
+    session.commit()
+
+    data = database.base.metadata.tables.keys()
+    for row in data:
+        print(row)
+
+    with open("backup-2019-08-17.sql", "r") as backup_file:
+        data = backup_file.readlines()
+
+    inserts = [line for line in data if line.startswith("INSERT")]
+
+    karma_values = []
+    for insert in inserts:
+        values = insert.split("VALUES", 1)[1]
+        if insert.startswith("INSERT INTO `bot_karma`"):
+            values = values[1:-2].replace('\'', '')
+            values = values.replace('(', '').replace(')', '')
+            values = values.split(',')
+            for i in range(0, len(values), 3):
+                karma_values.append(Karma(member_ID=values[i],
+                                          karma=values[i + 1]))
+        elif insert.startswith("INSERT INTO `bot_karma_giving`"):
+            values = values[1:-2].replace('\'', '')
+            values = values.replace('(', '').replace(')', '')
+            values = values.split(',')
+            for i in range(0, len(values), 4):
+                found = [karma for karma in karma_values
+                         if karma.member_ID == values[i]]
+                if found == []:
+                    karma_values.append(Karma(member_ID=values[i],
+                                              karma=0,
+                                              positive=values[i + 1],
+                                              negative=values[i + 2]))
+                elif len(found) == 1:
+                    found[0].positive = values[i + 1]
+                    found[0].negative = values[i + 2]
+                else:
+                    raise Exception("what the fuck is going on here")
+        elif insert.startswith("INSERT INTO `bot_karma_emoji`"):
+            values = values[1:-2].replace('\'', '')
+            values = values.replace('(', '').replace(')', '')
+            values = values.split(',')
+            for i in range(0, len(values), 2):
+                session.add(Karma_emoji(emoji_ID=values[i],
+                                        value=values[i + 1]))
+        elif insert.startswith("INSERT INTO `bot_permit`"):
+            values = values[1:-2]
+            values = values.replace('(', '').replace(')', '')
+            values = re.split(r',(?=\')', values)
+            values = [value.replace('\'', '') for value in values]
+            for i in range(0, len(values), 3):
+                session.add(Permit(login=values[i],
+                                   discord_ID=values[i + 2]))
+        elif insert.startswith("INSERT INTO `bot_valid_persons`"):
+            values = values[1:-2].replace('\'', '')
+            values = values.replace('(', '').replace(')', '')
+            values = values.split(',')
+            for i in range(0, len(values), 5):
+                session.add(Valid_person(login=values[i],
+                                         name=values[i + 1],
+                                         year=values[i + 2],
+                                         code=values[i + 3]
+                                         if values[i + 3] != "NULL" else None,
+                                         status=values[i + 4]))
+
+    for karma in karma_values:
+        session.add(karma)
+
+    session.commit()
+
+
+load_dump()
 
 config = config.Config
 messages = messages.Messages
