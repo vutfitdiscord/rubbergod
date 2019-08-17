205d7680cb315006fad086e44f4351e005404592
toaster192 <toastercz@gmail.com>
2019-08-17 10:05:14 +0200

    Finish converting database structure

---
+++ repository/database/verification.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String, Integer
+from repository.database import database
+
+
+class Permit(database.base):
+    __tablename__ = 'bot_permit'
+
+    login = Column(String, primary_key=True)
+    discord_ID = Column(String)
+
+
+class Valid_person(database.base):
+    __tablename__ = 'bot_valid_persons'
+
+    login = Column(String, primary_key=True)
+    name = Column(String)
+    year = Column(String)
+    code = Column(String)
+    status = Column(Integer)
