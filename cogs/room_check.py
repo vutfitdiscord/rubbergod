abff7d9319c8d55d7d58d08d628474561f402167
David Černý <cernydcer@gmail.com>
2020-01-08 17:41:37 +0100

    Minor fixes

--- cogs/room_check.py
+++ cogs/room_check.py
@@ -13,7 +13,7 @@ class RoomCheck():
         room = await self.get_room(message)
         if room is not None and room.id not in config.allowed_channels:
             await message.channel.send(utils.fill_message("bot_room_redirect",
-                                       config.bot_room, user=message.author.id))
+                                       user=message.author.id, bot_room=config.bot_room))
 
     async def get_room(self, message):
         guild = self.bot.get_guild(config.guild_id)
