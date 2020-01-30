98e90b7780e80be83622d9e53f260b645b159c2b
Peter Dragun <pdragun@redhat.com>
2020-01-30 17:36:17 +0100

    uppercase fix for review update

--- logic/rng.py
+++ logic/rng.py
@@ -42,7 +42,7 @@ class Rng:
 
     @staticmethod
     def week():
-        starting_week = 38  # School started at 5th week (winter in 37)
+        starting_week = 5  # School started at 5th week (winter in 37)
         week = date.today().isocalendar()[1]  # get actual week number
         stud_week = week - starting_week
         odd = "Lichý"
