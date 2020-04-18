2aa3aed10e82a3b685a8783c57d519a1cfcfc33f
toaster192 <toastercz@gmail.com>
2020-04-18 02:41:32 +0200

    ios feature upgrades

--- cogs/ios.py
+++ cogs/ios.py
@@ -19,20 +19,20 @@ def running_for(time):
     minutes = now.minute - int(time.split(':')[1])
     return hours * 60 + minutes
 
-def unchanged_for(date):
+def unchanged_for(date, format_str):
     now = datetime.datetime.now()
-    date = datetime.datetime.strptime(date, '%b %d %H:%M:%S')
-    return (now - date.replace(year=now.year)).seconds // 60
+    date = datetime.datetime.strptime(date, format_str)
+    return (now - date.replace(year=now.year)).total_seconds() // 60
 
 def parse_memory(memory):
     parsed = dict()
-    for line in memory.splitlines():
+    for line in memory.strip().splitlines():
         line = line.split()
         login = line[1]
         if login[0] != 'x':
             continue
         last_change = " ".join(line[-3:])
-        since_last_change = unchanged_for(last_change)
+        since_last_change = unchanged_for(last_change, '%b %d %H:%M:%S')
         if since_last_change > 10:
             if login not in parsed:
                 parsed[login] = list()
@@ -40,23 +40,43 @@ def parse_memory(memory):
     return parsed
 
 def parse_semaphores(semaphores):
+    semaphores, files = semaphores.split("soubory semaforu:\n")
     parsed = dict()
-    for line in semaphores.splitlines():
+    for line in semaphores.strip().splitlines():
         line = line.split()
         login = line[1]
         if login[0] != 'x':
             continue
         last_change = " ".join(line[-4:-1])
-        since_last_change = unchanged_for(last_change)
+        since_last_change = unchanged_for(last_change, '%b %d %H:%M:%S')
         if since_last_change > 10:
             if login not in parsed:
                 parsed[login] = list()
             parsed[login].append(since_last_change)
-    return parsed
+
+    parsed_files = dict()
+    for line in files.strip().splitlines():
+        line = line.split()
+        login = line[2]
+        if login[0] != 'x':
+            continue
+        last_change = " ".join(line[5:7])
+        name = line[7]
+        if "ADBE" in name:
+            continue
+        since_last_change = unchanged_for(last_change, "%m-%d %H:%M")
+        if since_last_change > 10 or login not in name:
+            if login not in parsed_files:
+                parsed_files[login] = [list(), False]
+            parsed_files[login][0].append(since_last_change)
+            if login not in name:
+                parsed_files[login][1] = True
+
+    return parsed, parsed_files
 
 def parse_processes(processes):
     parsed = dict()
-    for line in processes.splitlines():
+    for line in processes.strip().splitlines():
         line = line.split()
         login = line[0]
         time = line[8]
@@ -81,9 +101,12 @@ class IOS(commands.Cog):
         output, error = process.communicate()
         memory, rest = output.decode('utf-8').split("semafory:\n")
         semaphores, processes = rest.split("procesy:\n")
-        parsed_memory = parse_memory(memory)
-        parsed_semaphores = parse_semaphores(semaphores)
-        parsed_processes = parse_processes(processes)
+        try:
+            parsed_memory = parse_memory(memory)
+            parsed_semaphores, parsed_files = parse_semaphores(semaphores)
+            parsed_processes = parse_processes(processes)
+        except IndexError:
+            await ctx.send("Toastere mas bordel v parsovani")
         if parsed_memory != dict():
             for login, array in parsed_memory.items():
                 user = session.query(Permit).\
@@ -115,6 +138,30 @@ class IOS(commands.Cog):
                                    " semaforu, lezicich tam prumerne " +
                                    str(avg_time) + " minut, ty prase")
 
+        if parsed_files != dict():
+            for login, array in parsed_files.items():
+                user = session.query(Permit).\
+                    filter(Permit.login == login).one_or_none()
+                login_not_in_name = array[1]
+                array = array[0]
+                count = len(array)
+                avg_time = sum(array) // count
+
+                if user is None:
+                    await ctx.send("Soubory semaforu nechava nejaky " +
+                                   login + " co neni na serveru")
+                else:
+                    await ctx.send(utils.generate_mention(user.discord_ID) +
+                                   " mas na merlinovi(/dev/shm) " +
+                                   str(count) + " souboru semaforu")
+                    if avg_time > 9:
+                        await ctx.send("Lezi ti tam prumerne uz " +
+                                       str(avg_time) + " minut, ty prase")
+                    if login_not_in_name:
+                        await ctx.send("Nemas v nazvu tvuj login takze muzes" +
+                                        " mit kolize s ostatnima, ty prase")
+
+
         if parsed_processes != dict():
             for login, array in parsed_processes.items():
                 user = session.query(Permit).\
@@ -131,8 +178,13 @@ class IOS(commands.Cog):
                                    str(avg_time) + " minut, ty prase")
         
         if (parsed_memory == dict() and parsed_semaphores == dict()
-                and parsed_processes == dict()):
+                and parsed_processes == dict() and parsed_files == dict()):
             await ctx.send("Uklizeno <:HYPERS:493154327318233088>")
+        else:
+            await ctx.send("Pokud nevite jak po sobe uklidit checknete: " +
+                           "https://discordapp.com/channels/" + 
+                           "461541385204400138/534431057001316362/" +
+                           "698701631495340033")
 
 
     @ios.error
