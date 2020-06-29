9b02f83cef1e3acb6d9ff43960c0db5939aeca20
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-06-29 23:46:10 +0200

    Week: Use embed

--- cogs/week.py
+++ cogs/week.py
@@ -1,9 +1,7 @@
 from datetime import date
 
-import discord
 from discord.ext import commands
 
-import utils
 from config import config, messages
 
 config = config.Config
@@ -15,21 +13,20 @@ class week(commands.Cog):
         self.bot = bot
 
     @commands.cooldown(rate=2, per=20.0, type=commands.BucketType.user)
-    @commands.command()
-    async def week(self, ctx, *args):
-        if args and args[0] == "set":
-            if ctx.author.id == config.admin_id:
-                config.starting_week = int(args[1])
-                await ctx.send(("Starting week set to {}").format(args[1]))
-
-        week = date.today().isocalendar()[1]  # get actual week number
-        stud_week = week - config.starting_week
-        odd = "Lichý"
-        even = "Sudý"
-        cal_type = even if week % 2 == 0 else odd
+    @commands.command(aliases=["tyden", "týden", "tyzden", "týždeň"])
+    async def week(self, ctx: commands.Context):
+        """See if the current week is odd or even"""
+        cal_week = date.today().isocalendar()[1]
+        stud_week = cal_week - config.starting_week
+        even, odd = "sudý", "lichý"
+        cal_type = even if cal_week % 2 == 0 else odd
         stud_type = even if stud_week % 2 == 0 else odd
-        await ctx.send("Cal\t{}\t{}\nStd\t{}\t{}".format(cal_type, week,
-                                                stud_type, stud_week))
+
+        embed = self.embed(ctx=ctx)
+        embed.add_field(name="Studijní", value="{} ({})".format(stud_type, stud_week))
+        embed.add_field(name="Kalendářní", value="{} ({})".format(cal_type, cal_week))
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(week(bot))
