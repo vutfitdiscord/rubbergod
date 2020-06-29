e76dd0c1d435c2e979726008575c862f58ded213
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-06-29 23:42:42 +0200

    Weather: Remove unused imports, fix flake8 warnings

--- cogs/weather.py
+++ cogs/weather.py
@@ -1,10 +1,8 @@
 import requests
-import json
 
 import discord
 from discord.ext import commands
 
-import utils
 from config import config, messages
 
 config = config.Config
@@ -15,18 +13,23 @@ class weather(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['pocasi', 'pocasie'])
+    @commands.command(aliases=["pocasi", "pocasie", "počasí", "počasie"])
     async def weather(self, ctx, *args):
         token = config.weather_token
         city = "Brno"
-        if(len(args) != 0):
-            city = ' '.join(map(str, args))
-        url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=cz&appid=" + token
+        if len(args) != 0:
+            city = " ".join(map(str, args))
+        url = (
+            "http://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&lang=cz&appid="
+            + token
+        )
         res = requests.get(url).json()
-        
-        if(str(res["cod"]) == "200"):
+
+        if str(res["cod"]) == "200":
             description = "Aktuální počasí v městě " + res["name"] + ", " + res["sys"]["country"]
-            embed=discord.Embed(title="Počasí", description=description)
+            embed = discord.Embed(title="Počasí", description=description)
             image = "http://openweathermap.org/img/w/" + res["weather"][0]["icon"] + ".png"
             embed.set_thumbnail(url=image)
             weather = res["weather"][0]["main"] + " ( " + res["weather"][0]["description"] + " ) "
@@ -44,12 +47,15 @@ class weather(commands.Cog):
             embed.add_field(name="Oblačnost", value=clouds, inline=True)
             embed.add_field(name="Viditelnost", value=visibility, inline=True)
             await ctx.send(embed=embed)
-        elif(str(res["cod"]) == "404"):
+        elif str(res["cod"]) == "404":
             await ctx.send("Město nenalezeno")
-        elif(str(res["cod"]) == "401"):
+        elif str(res["cod"]) == "401":
             await ctx.send("Rip token -> Rebel pls fix")
         else:
-            await ctx.send("Město nenalezeno! <:pepeGun:484470874246742018> (" + res["message"] + ")")
+            await ctx.send(
+                "Město nenalezeno! <:pepeGun:484470874246742018> (" + res["message"] + ")"
+            )
+
 
 def setup(bot):
     bot.add_cog(weather(bot))
