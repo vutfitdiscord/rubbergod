cdebee747234a572ff5f6c54997d2d21d1809eba
sinus-x <14353790+sinus-x@users.noreply.github.com>
2020-07-10 12:27:13 +0200

    Fix strings in messages.py

--- config/messages.py
+++ config/messages.py
@@ -5,26 +5,25 @@ class Messages:
     prefix = Config.default_prefix
 
     server_warning = "Tohle funguje jen na VUT FIT serveru."
-    karma_get_missing = "Toaster pls, měls bordel v DB. Musel jsem za tebe uklidit."
-    missing_perms = "Na tohle nemas prava. {user}"
-    acl_help = "{user}, Pouziti:\n`!acl {{action}} {{table}} [args]`\n"\
+    karma_get_missing = "Toaster pls, měl jsi bordel v DB. Musel jsem za tebe uklidit."
+    missing_perms = "Na tohle nemáš práva, {user}"
+    acl_help = "{user}, Použití:\n`!acl {{action}} {{table}} [args]`\n"\
                "action: add, edit, del nebo list\n"\
                "table: group, rule, role nebo user\n"\
-               "args zalezi na zvolene akci a tabulce, napriklad kdyz chcete"\
-               "pridat groupu, musite zadat nazev groupy a nepovinne take "\
-               "ID rodice jako argumnet"  # TODO: Correct to proper czech
-    acl_add_group = "Group vytvorena."
-    acl_edit_group = "Group zmenena."
-    acl_del_group = "Group smazana."
-    acl_add_rule = "Pravidlo vytvoreno."
-    acl_edit_rule = "Pravidlo zmeneno."
-    acl_del_rule = "Pravidlo smazano."
-    acl_add_role = "Vyjimka pro roli pridana."
-    acl_edit_role = "Vyjimka pro roli upravena."
-    acl_del_role = "Vyjimka pro roli smazana."
-    acl_add_user = "Vyjimka pro uzivatele pridana."
-    acl_edit_user = "Vyjimka pro uzivatele upravena."
-    acl_del_user = "Vyjimka pro uzivatele smazana."
+               "Argumenty záleží na zvolené akci a tabulce: "\
+               "pro přidání groupy musíte zadat název groupy a ID rodiče jako nepovinný argument."
+    acl_add_group = "Group vytvořena."
+    acl_edit_group = "Group změněna."
+    acl_del_group = "Group smazána."
+    acl_add_rule = "Pravidlo vytvořeno."
+    acl_edit_rule = "Pravidlo změněno."
+    acl_del_rule = "Pravidlo smazáno."
+    acl_add_role = "Výjimka pro roli přidána."
+    acl_edit_role = "Výjimka pro roli upravena."
+    acl_del_role = "Výjimka pro roli smazána."
+    acl_add_user = "Výjimka pro uživatele přidána."
+    acl_edit_user = "Výjimka pro uživatele upravena."
+    acl_del_user = "Výjimka pro uživatele smazána."
     no_such_command = "Takový příkaz neznám. <:sadcat:576171980118687754>"
     spamming = "{user} Nespamuj tolik <:sadcat:576171980118687754>"
     insufficient_rights = "{user}, na použití tohoto příkazu nemáš právo."
@@ -40,7 +39,7 @@ class Messages:
     kachna_grillbot = "O Kachnu se teď stará Grillbot " \
                       "<:feelsWowMan:493152294712377354> Použij $kachna."
 
-    karma = "{user} Karma uzivatele `{target}` je: **{karma}** " \
+    karma = "{user} Karma uživatele `{target}` je: **{karma}** " \
             "(**{order}.**)\nA rozdal:\n" \
             "**{karma_pos}** pozitivní karmy " \
             "(**{karma_pos_order}.**)\n" \
@@ -48,7 +47,7 @@ class Messages:
             "(**{karma_neg_order}.**)"
 
     karma_invalid_command = "Neznámý karma příkaz."
-    karma_vote_format = "Neočekávám argument. " \
+    karma_vote_format = "Neočekávám žádný argument. " \
                         "Správný formát: `" + prefix + "karma vote`"
     karma_vote_message_hack = "Hlasování o karma ohodnocení emotu"
     karma_vote_message = karma_vote_message_hack + " {emote}"
@@ -57,17 +56,17 @@ class Messages:
                       "**{minimum}**."
     karma_vote_result = "Výsledek hlasování o emotu {emote} " \
                         "je {result}."
-    karma_vote_notpassed = "Hlasovani o emotu {emote} neprošlo\n" \
-                           "Aspoň {minimum} hlasů potřeba."
+    karma_vote_notpassed = "Hlasovani o emotu {emote} neprošlo.\n" \
+                           "Bylo třeba aspoň {minimum} hlasů."
     karma_vote_allvoted = "Už se hlasovalo o všech emotech."
-    karma_revote_format = "Očekávám pouze formát: " \
+    karma_revote_format = "Očekávám pouze formát " \
                           "`" + prefix + "karma revote [emote]`"
     karma_emote_not_found = "Emote jsem na serveru nenašel."
     karma_get_format = "Použití:\n" \
                        "`" + prefix + "karma get`: " \
                        "vypíše všechny emoty s hodnotou.\n" \
                        "`" + prefix + "karma get [emote]`: " \
-                       "vrátí hodnotu daného emotu."
+                       "zobrazí hodnotu daného emotu."
     karma_get = "Hodnota {emote} je {value}."
     karma_get_emote_not_voted = "{emote} není ohodnocen."
     karma_give_format = "Toaster pls, formát je " \
@@ -83,33 +82,25 @@ class Messages:
     karma_lederboard_offser_error = "{user} Špatný offset, zadej kladné číslo"
     karma_web_title = "Celý leaderboard"
     karma_web = "https://karma.grillbot.cloud/"
-    karma_transfer_format = "Ten příkaz voláš špatně.\n" \
-                            "Formát je `" + prefix + "karma transfer [od koho] [komu]`"
-    karma_transfer_complete = "Karma byla úspěšně převedena.\n" \
-                              "Převedl jsem z uživatele `{from_user}` na `{to_user}`:\n" \
+    karma_transfer_format = "Správný formát je `" + prefix + "karma transfer [od koho] [komu]`"
+    karma_transfer_complete = "Karma byla úspěšně převedena z `{from_user}` na `{to_user}`:\n" \
                               "Množství karmy: **{karma}**\n" \
                               "Množství pozitivně rozdané karmy: **{positive}** \n" \
                               "Množství negativně rozdané karmy: **{negative}**"
 
     role_add_denied = "{user}, na přidání role {role} nemáš právo."
-    role_remove_denied = "{user}, " \
-                         "na odebrání role {role} nemáš právo."
+    role_remove_denied = "{user}, na odebrání role {role} nemáš právo."
     role_invalid_line = "{user}, řádek `{line}` je neplatný."
     role_format = "{user}, použij `!god`."
-    role_not_on_server = "Nepíšeš na serveru, " \
-                         "takže předpokládám, " \
-                         "že myslíš role VUT FIT serveru."
+    role_not_on_server = "Nepíšeš na serveru, takže předpokládám, že myslíš role VUT FIT serveru."
     role_not_role = "{user}, {not_role} není role."
-    role_invalid_emote = "{user}, {not_emote} " \
-                         "pro roli {role} není emote."
+    role_invalid_emote = "{user}, {not_emote} pro roli {role} není emote."
 
     rng_generator_format = "Použití: `" + prefix + "roll x [y]`\n" \
-                           "x, y je rozmezí čísel,\n" \
-                           "x, y jsou celá čísla,\n" \
+                           "rozmezí x, y jsou celá čísla,\n" \
                            "pokud y není specifikováno, " \
                            "je považováno za 0."
-    rng_generator_format_number = "{user}, zadej dvě celá čísla, " \
-                                  "**integers**."
+    rng_generator_format_number = "{user}, zadej dvě celá čísla, **integers**."
 
     rd_too_many_dice_in_group = "Příliš moc kostek v jedné " \
                                 "skupině, maximum je {maximum}."
@@ -117,7 +108,7 @@ class Messages:
                              "maximum je {maximum}."
     rd_too_many_dice_groups = "Příliš moc skupin kostek, " \
                               "maximum je {maximum}."
-    rd_format = "Chybná syntax hodu ve skupině {group}."
+    rd_format = "Chybná syntaxe hodu ve skupině {group}."
     rd_help = "Formát naleznete na " \
               "https://wiki.roll20.net/Dice_Reference\n" \
               "Implementovány featury podle obsahu: **8. Drop/Keep**"
@@ -149,10 +140,9 @@ class Messages:
                            "aby to udělal manuálně, " \
                            "jsi shady (Year: {year})"
     verify_verify_success = "{user} Gratuluji, byl jsi verifikován!"
-    verify_post_verify_info = "Channely dobre navstivit po verifikaci:\n" \
+    verify_post_verify_info = "Podívej se do kanálů:\n" \
                               "<#591384273051975683> Pro pridani roli\n" \
-                              "<#489461089432633346> Pro pravidla " \
-                              "a dalsi info"
+                              "<#489461089432633346> Pro pravidla a další info"
 
     verify_verify_not_found = "{user} Login nenalezen nebo " \
                               "jsi už tímhle krokem prošel " \
@@ -185,21 +175,21 @@ class Messages:
     review_add_format = prefix + "reviews add {ZkratkaPredmetu} {Tier (0-4, 0 je TOP)} (VolitelnyText)\n" \
         "Pro anonymní příspěvek použijte DM.\nNapříklad:\n`" + \
         prefix + "reviews add IZP 2 text recenze`"
-    review_wrong_subject = "Nesprávná zkratka předmětu"
-    review_tier = "Tier je z rozsahu 0-4, kde 0 je nejlepší"
-    review_text_len = "Maximální počet znaků v části 'text' je 1024"
-    review_added = "Hodnocení předmětu bylo přidáno"
-    reviews_page_e = "Zavolej znova reviews pro aktualizaci"
+    review_wrong_subject = "Nesprávná zkratka předmětu."
+    review_tier = "Tier je z rozsahu 0-4, kde 0 je nejlepší."
+    review_text_len = "Maximální počet znaků v části 'text' je 1024."
+    review_added = "Hodnocení předmětu bylo přidáno."
+    reviews_page_e = "Zavolej reviews znovu pro aktualizaci."
 
     review_get_format = prefix + "reviews [zkratka předmětu]"
     review_remove_format = prefix + "reviews remove [zkratka předmětu]"
     review_remove_format_admin = prefix + \
         "reviews remove [zkratka předmětu, id + číslo]"
     review_remove_id_format = "reviews remove id [id]"
-    review_remove_success = "Hodnocení předmětu bylo odebráno"
-    review_remove_error = "Hodnocení předmětu nebylo nalezeno"
+    review_remove_success = "Hodnocení předmětu bylo odebráno."
+    review_remove_error = "Hodnocení předmětu nebylo nalezeno."
     review_add_denied = "{user}, na přidání hodnocení předmětu nemáš právo."
-    review_not_on_server = "{user}, na použití tohto příkazu musíš být na Fitwide serveri."
+    review_not_on_server = "{user}, na použití tohto příkazu musíš být na FITwide serveru."
     subject_format = prefix + "subject [add, remove] [zkratka předmětu]"
 
     pr_meme = "https://github.com/Toaster192/rubbergod/pulls"
@@ -213,48 +203,37 @@ class Messages:
 
     repost_title = "Nápověda"
     repost_description = "{user}, shoda **{value}**!"
-    repost_content = "Pokud obrázek je repost, dej mu ♻️.\nJestli není, klikni tady na ❎ a při {limit} takových reakcích se toho upozornění smaže."
+    repost_content = "Pokud je obrázek repost, dej mu ♻️.\nJestli není, klikni tady na ❎ a při {limit} takových reakcích se toho upozornění smaže."
 
     on_ready_message = "<:peepowave:693070888546861096>"
 
-    cog_is_loaded = 'Toto rozšíření `{cog}` mám již v provozu.'
-    cog_unloaded = 'Rozšíření `{cog}` uvolněno.'
+    cog_is_loaded = 'Toto rozšíření `{cog}` je již načtené.'
+    cog_unloaded = 'Rozšíření `{cog}` odebráno.'
     cog_loaded = 'Rozšíření `{cog}` načteno.'
     cog_is_unloaded = 'Toto rozšíření `{cog}` není načteno'
     cog_cannot_be_unloadable = 'Toto rozšíření `{cog}` je neodebratelné.'
-    cog_reloaded = 'Rozšíření `{cog}` bylo přenačteno.'
+    cog_reloaded = 'Rozšíření `{cog}` bylo načteno znovu.'
 
-    info = [[('karma', 'Vypíše vaši karmu, kolik pozitivní a negativní karmy'
-                       ' jste rozdali.'),
-             ('karma stalk [user]', 'Vypíše karmu uživatele, kolik pozitivní '
-                                    'a negativní karmy rozdal.'),
-             ('karma get',
-              'Vypíše, které emoty mají hodnotu 1 a -1.'),
-             ('karma get [emote]',
-              'Vrátí karma hodnotu emotu.'),
-             ('karma vote',
-              'Odstartuje hlasování o hodnotě zatím neohodnoceného emotu.'),
-             ('karma revote [emote]',
-              'Odstartuje hlasování o nové hodnotě emotu.'),
-             ('karma message [url, id]',
-              'Zobrazí karmu získanou za zprávu')],
+    info = [[('karma', 'Vypíše karmu vaši, rozdanou a odebranou'),
+             ('karma stalk [user]', 'Vypíše karmu uživatele, karmu rozdanou a odebranou.'),
+             ('karma get', 'Vypíše, které emoty mají hodnotu 1 a -1.'),
+             ('karma get [emote]', 'Vrátí karma hodnotu emotu.'),
+             ('karma vote', 'Odstartuje hlasování o hodnotě zatím neohodnoceného emotu.'),
+             ('karma revote [emote]', 'Odstartuje hlasování o nové hodnotě emotu.'),
+             ('karma message [url, id]', 'Zobrazí karmu získanou za zprávu')],
             [('leaderboard [offset]', 'Karma leaderboard'),
              ('bajkarboard [offset]', 'Karma leaderboard reversed'),
-             ('givingboard [offset]',
-              'Leaderboard rozdávání pozitivní karmy.'),
+             ('givingboard [offset]', 'Leaderboard rozdávání pozitivní karmy.'),
              ('ishaboard [offset]', 'Leaderboard rozdávání negativní karmy.'),
              (review_add_format[1:], 'Přidá recenzi na předmět.'),
              (review_get_format[1:], 'Vypíše recenze na vybraný předmět.'),
              (review_remove_format[1:], 'Odstraní hodnocení.'),
              ('vote', 'Zahájí hlasování.')],
-            [('roll X Y',
-              'Vygeneruje náhodné celé číslo z intervalu <**X**, **Y**>.'),
+            [('roll X Y', 'Vygeneruje náhodné celé číslo z intervalu <**X**, **Y**>.'),
              ('flip', 'Hodí mincí'),
-             ('pick *Is foo bar? Yes No Maybe*',
-              'Vybere jedno ze slov za otazníkem.'),
+             ('pick *Is foo bar? Yes No Maybe*', 'Vybere jedno ze slov za otazníkem.'),
              ('diceroll', 'Všechno možné házení kostkami.'),
-             ('week', 'Vypíše, kolikátý je zrovna týden '
-                      'a jestli je sudý nebo lichý.'),
+             ('week', 'Vypíše, kolikátý je zrovna týden a jestli je sudý nebo lichý.'),
              ('uhoh', 'Vypíše počet uh ohs od spuštění.'),
              ('uptime', 'Vypíše čas spuštění a čas uplynulý od spuštění.'),
              ('kachna', 'Vypíše čas do otevření kachny.'),
