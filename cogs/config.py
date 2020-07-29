c3e34e6fb497ab813571112cce99e2e9447e29a6
Peter Dragun <pdragun@redhat.com>
2020-07-30 00:26:26 +0200

    toml config

--- cogs/config.py
+++ cogs/config.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import re
 import os
+import toml
 from datetime import datetime
 
 import utils
@@ -10,7 +11,6 @@ from config.messages import Messages
 
 
 class DynamicConfig(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
 
@@ -25,14 +25,14 @@ class DynamicConfig(commands.Cog):
         if key is None:
             await ctx.send(Messages.config_set_format)
             return
-        await self.change_value(ctx, key, value, False)
+        await self.change_value(ctx, key, list(value), False)
 
     @config.command()
     async def update(self, ctx, key=None, *value):
         if key is None:
             await ctx.send(Messages.config_update_format)
             return
-        await self.change_value(ctx, key, value, True)
+        await self.change_value(ctx, key, list(value), True)
 
     @config.command(name="list")
     async def list_all(self, ctx):
@@ -53,81 +53,51 @@ class DynamicConfig(commands.Cog):
             await ctx.send(Messages.config_wrong_key)
             return
         value = getattr(Config, key)
-        embed = discord.Embed(title=key, description=value)
+        embed = discord.Embed(title=key, description=str(value))
         await ctx.send(embed=embed)
 
     @config.command()
     async def backup(self, ctx):
-        config_path = os.path.dirname(__file__)[:-4] + "config/config.py"
-        with open(config_path, "r") as fd:
-            lines = fd.readlines()
-        config_path = config_path[:-3] + "_backup_" + str(datetime.now().date()) + ".py"
+        config_path = os.path.dirname(__file__)[:-4] + "config/config_backup_"
+        config_path += str(datetime.now().date()) + ".toml"
         with open(config_path, "w+") as fd:
-            fd.writelines(lines)
+            toml.dump(Config.toml_dict, fd)
         await ctx.send(Messages.config_backup_created)
 
     async def change_value(self, ctx, key, value, append):
-        config_path = os.path.dirname(__file__)[:-4] + "config/config.py"
-        with open(config_path, "r") as fd:
-            lines = fd.readlines()
         if not hasattr(Config, key):
+            ctx.send(Messages.config_wrong_key)
+            return
+        config_path = os.path.dirname(__file__)[:-4] + "config/config.toml"
+        for section in Config.toml_dict:
+            if key in Config.toml_dict[section]:
+                attr = getattr(Config, key)
+                if isinstance(attr, list):
+                    if isinstance(attr[0], int):
+                        for idx, item in enumerate(value):
+                            try:
+                                value[idx] = int(item)
+                            except ValueError:
+                                await ctx.send(Messages.config_wrong_type)
+                                return
+                    if append:
+                        value = attr + value
+                elif isinstance(attr, tuple) and append:
+                    value = tuple(list(attr) + key)
+                elif isinstance(attr, int):
+                    try:
+                        value = int(value[0])
+                    except ValueError:
+                        await ctx.send(Messages.config_wrong_type)
+                        return
+                Config.toml_dict[section][key] = value
+                break
+        else:
             await ctx.send(Messages.config_wrong_key)
             return
-        attrb = getattr(Config, key)
-        for i, line in enumerate(lines):
-            if re.search("    " + key + r".*", line):
-                if isinstance(attrb, int):
-                    lines[i] = "    " + key + " = " + value
-                    value = int(value)
-                elif isinstance(attrb, str):
-                    lines[i] = "    " + key + " = '" + value + "'"
-                elif isinstance(attrb, list):
-                    # look for ] and read whole list in var
-                    list_str = ""
-                    # read whole list
-                    lines_cnt = 0
-                    for list_line in lines[i:]:
-                        list_str += list_line
-                        lines_cnt += 1
-                        if re.match(r".*].*", list_line):
-                            break
-                    line_str = "".join(list_str.split())
-                    # remove setting name
-                    line_str = line_str.split("[")[1]
-                    # remove ]
-                    line_str = line_str[:-1]
-                    line_list = line_str.split(",")
-                    value = list(value)
-                    # add quotes to string list
-                    if isinstance(value[0], str):
-                        for j, item in enumerate(value):
-                            value[j] = '"' + item + '"'
-                    if append:
-                        line_list += value
-                    else:
-                        line_list = value
-                    # remove duplicates
-                    value = list(dict.fromkeys(line_list))
-                    # print list back to config file with correct formatting
-                    lines_out = ["    " + key + " = ["]
-                    current_line = "        "
-                    for item in value:
-                        if len(current_line) + len(item) > 100:
-                            lines_out.append(current_line[:-1])
-                            current_line = "        "
-                        else:
-                            current_line += item + ", "
-                    lines_out[-1] = lines_out[-1][:-1]
-                    lines_out.append("    ]")
-                    del lines[i : i + lines_cnt]
-                    lines = lines[:i] + lines_out + lines[i:]
-                else:
-                    await ctx.send("Wrong type")
-                    return
-                break
-        with open(config_path, "w") as fd:
-            fd.writelines(lines)
         setattr(Config, key, value)
+        with open(config_path, "w+") as fd:
+            toml.dump(Config.toml_dict, fd)
         await ctx.send(Messages.config_updated)
 
 
--- config/app_config.py
+++ config/app_config.py
@@ -1,21 +1,28 @@
 from typing import List
-
-import config.config as ConfigOverride
-
-import importlib.util
-template_spec = importlib.util.spec_from_file_location('module.name', 'config/config.template.py')
-config_template = importlib.util.module_from_spec(template_spec)
-template_spec.loader.exec_module(config_template)
+import toml
 
 
-def get_attr(attr_key: str):
+toml_dict: dict = toml.load("config/config.toml", _dict=dict)
+
+
+def get_attr(section: str, attr_key: str):
     """
     Helper method for getting values from config override or config template.
     """
-    if not hasattr(ConfigOverride.Config, attr_key):
-        return getattr(config_template.Config, attr_key)
+    try:
+        return toml_dict[section][attr_key]
+    except KeyError:
+        return toml.load("config/config.template.toml", _dict=dict)[section][attr_key]
 
-    return getattr(ConfigOverride.Config, attr_key)
+
+def eval_channels(channels: list):
+    """
+    Evaluate channel 'config variable name' to ID
+    """
+    for idx, channel_name in enumerate(channels):
+        if isinstance(channel_name, str):
+            channels[idx] = get_attr("channels", channel_name)
+    return channels
 
 
 class Config:
@@ -24,92 +31,97 @@ class Config:
     It checks value from config override and if not exists that will be take from config template.
     """
 
-    # Authorization
-    key: str = get_attr('key')
+    # String representation of toml config
+    toml_dict = toml_dict
 
-    # Verification
-    verification_role: str = get_attr('verification_role')
-    verification_role_id: int = get_attr('verification_role_id')
+    # Authorization
+    key: str = get_attr("base", "key")
 
     # Base information
-    admin_id: int = get_attr('admin_id')
-    guild_id: int = get_attr('guild_id')
-
-    # Verification email sender settings
-    email_name: str = get_attr('email_name')
-    email_addr: str = get_attr('email_addr')
-    email_smtp_server: str = get_attr('email_smtp_server')
-    email_smtp_port: str = get_attr('email_smtp_port')
-    email_pass: str = get_attr('email_pass')
+    admin_id: int = get_attr("base", "admin_id")
+    guild_id: int = get_attr("base", "guild_id")
 
     # Database
-    db_string: str = get_attr('db_string')
+    db_string: str = get_attr("database", "db_string")
 
     # Base bot behavior
-    command_prefix: tuple = get_attr('command_prefix')
-    default_prefix: str = get_attr('default_prefix')
-    ignored_prefixes: tuple = get_attr('ignored_prefixes')
+    command_prefix: tuple = tuple(get_attr("base", "command_prefix"))
+    default_prefix: str = get_attr("base", "default_prefix")
+    ignored_prefixes: tuple = tuple(get_attr("base", "ignored_prefixes"))
+
+    # Verification
+    verification_role: str = get_attr("verification", "role")
+    verification_role_id: int = get_attr("verification", "role_id")
+
+    # Verification email sender settings
+    email_name: str = get_attr("email", "name")
+    email_addr: str = get_attr("email", "addr")
+    email_smtp_server: str = get_attr("email", "smtp_server")
+    email_smtp_port: str = get_attr("email", "smtp_port")
+    email_pass: str = get_attr("email", "pass")
 
     # Extensions loaded on bot start
-    extensions: List[str] = get_attr('extensions')
+    extensions: List[str] = get_attr("extensions", "extensions")
 
     # Roll dice
-    max_dice_at_once: int = get_attr('max_dice_at_once')
-    dice_before_collation: int = get_attr('dice_before_collation')
-    max_dice_groups: int = get_attr('max_dice_groups')
-    max_dice_sides: int = get_attr('max_dice_sides')
+    max_dice_at_once: int = get_attr("random", "max_dice_at_once")
+    dice_before_collation: int = get_attr("random", "dice_before_collation")
+    max_dice_groups: int = get_attr("random", "max_dice_groups")
+    max_dice_sides: int = get_attr("random", "max_dice_sides")
 
     # Karma
-    karma_ban_role_id: int = get_attr('karma_ban_role_id')
-    karma_banned_channels: List[int] = get_attr('karma_banned_channels')
-    grillbot_leaderboard_size: int = get_attr('grillbot_leaderboard_size')
+    karma_ban_role_id: int = get_attr("karma", "ban_role_id")
+    karma_banned_channels: List[int] = get_attr("karma", "banned_channels")
+    grillbot_leaderboard_size: int = get_attr("karma", "grillbot_leaderboard_size")
 
     # Voting
-    vote_minimum: int = get_attr('vote_minimum')
-    vote_minutes: int = get_attr('vote_minutes')
+    vote_minimum: int = get_attr("vote", "minimum")
+    vote_minutes: int = get_attr("vote", "minutes")
 
     # Pin emoji count to pin
-    pin_count: int = get_attr('pin_count')
-    pin_banned_channels: List[int] = get_attr('pin_banned_channels')
+    pin_count: int = get_attr("autopin", "count")
+    pin_banned_channels: List[int] = get_attr("autopin", "banned_channels")
 
     # Special channel IDs
-    log_channel: int = get_attr('log_channel')
-    bot_dev_channel: int = get_attr('bot_dev_channel')
-    vote_room: int = get_attr('vote_room')
-    bot_room: int = get_attr('bot_room')
-    mod_room: int = get_attr('mod_room')
+    log_channel: int = get_attr("channels", "log_channel")
+    bot_dev_channel: int = get_attr("channels", "bot_dev_channel")
+    vote_room: int = get_attr("channels", "vote_room")
+    bot_room: int = get_attr("channels", "bot_room")
+    mod_room: int = get_attr("channels", "mod_room")
 
     # Bot rooms
-    allowed_channels: List[int] = get_attr('allowed_channels')
+    allowed_channels: List[int] = eval_channels(
+        get_attr("channels", "allowed_channels")
+    )
 
-    # String constants
-    role_string: str = get_attr('role_string')
-    role_channels: List[int] = get_attr('role_channels')
-    hug_emojis: List[str] = get_attr('hug_emojis')
+    # Roles
+    role_string: str = get_attr("role", "string")
+    role_channels: List[int] = get_attr("role", "channels")
 
     # Subjects shortcuts
-    subjects: List[str] = get_attr('subjects')
-    reviews_forbidden_roles: List[int] = get_attr('reviews_forbidden_roles')
+    subjects: List[str] = get_attr("review", "subjects")
+    reviews_forbidden_roles: List[int] = get_attr("review", "forbidden_roles")
 
     # How many people to print if the limit argument is not specified
-    rolehoarder_default_limit: int = get_attr('rolehoarder_default_limit')
+    rolehoarder_default_limit: int = get_attr("rolehoarder", "default_limit")
 
+    # memes
+    hug_emojis: List[str] = get_attr("meme", "hugs_emojis")
     # Arcas
-    arcas_id: int = get_attr('arcas_id')
-    arcas_delay: int = get_attr('arcas_delay')  # Value is in hours
-
+    arcas_id: int = get_attr("meme", "arcas_id")
+    arcas_delay: int = get_attr("meme", "arcas_delay")  # Value is in hours
     # uh oh
-    uhoh_string: str = get_attr('uhoh_string')
+    uhoh_string: str = get_attr("meme", "uhoh_string")
 
     # grillbot
-    grillbot_id: int = get_attr('grillbot_id')
+    grillbot_id: int = get_attr("grillbot", "id")
 
     # weather token to openweather API
-    weather_token: str = get_attr('weather_token')
+    weather_token: str = get_attr("weather", "token")
 
     # warden
-    duplicate_limit: int = get_attr('duplicate_limit')
-    deduplication_channels: List[int] = get_attr('deduplication_channels')
+    duplicate_limit: int = get_attr("warden", "duplicate_limit")
+    deduplication_channels: List[int] = get_attr("warden", "deduplication_channels")
 
     # week command
-    starting_week: int = get_attr('starting_week')
+    starting_week: int = get_attr("week", "starting_week")
