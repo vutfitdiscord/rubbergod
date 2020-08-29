7292bcd9bb83cd12d1aee22195eac3f7d4139e46
Peter Dragun <pdragun@redhat.com>
2020-08-29 23:12:18 +0200

    print ctx on error

--- cogs/base.py
+++ cogs/base.py
@@ -21,44 +21,6 @@ class Base(commands.Cog):
         self.bot = bot
         self.check = room_check.RoomCheck(bot)
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        # The local handlers so far only catch bad arguments so we still
-        # want to print the rest
-        if (isinstance(error, commands.BadArgument) or
-           isinstance(error, commands.errors.CheckFailure) or
-           isinstance(error, commands.errors.MissingAnyRole) or
-           isinstance(error, commands.errors.MissingRequiredArgument)) and \
-           hasattr(ctx.command, 'on_error'):
-            return
-
-        if isinstance(error, commands.UserInputError):
-            await ctx.send("Chyba ve vstupu")
-            return
-
-        if isinstance(error, commands.CommandNotFound):
-            prefix = ctx.message.content[:1]
-            if prefix not in config.ignored_prefixes:
-                await ctx.send(messages.no_such_command)
-            return
-
-        if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(utils.fill_message("spamming", user=ctx.author.id))
-            return
-
-        if isinstance(error, utils.NotHelperPlusError):
-            await ctx.send(messages.helper_plus_only)
-            return
-
-        output = "Ignoring exception in command {}:\n".format(ctx.command)
-        output += "".join(traceback.format_exception(type(error), error, error.__traceback__))
-        channel = self.bot.get_channel(config.bot_dev_channel)
-        print(output)
-        output = utils.cut_string(output, 1900)
-        if channel is not None:
-            for message in output:
-                await channel.send("```\n" + message + "\n```")
-
     @commands.cooldown(rate=2, per=20.0, type=commands.BucketType.user)
     @commands.command()
     async def uptime(self, ctx):
