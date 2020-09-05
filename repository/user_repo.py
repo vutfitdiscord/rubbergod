8bfa34607f6f4784493842993542d655b27032a7
Peter Dragun <pdragun@redhat.com>
2020-09-05 13:25:52 +0200

    add absolvent cog

--- repository/user_repo.py
+++ repository/user_repo.py
@@ -10,8 +10,7 @@ class UserRepository(BaseRepository):
 
     def save_sent_code(self, login: str, code: str):
         """Updates a specified login with a new verification code"""
-        person = session.query(Valid_person).\
-            filter(Valid_person.login == login).one_or_none()
+        person = session.query(Valid_person).filter(Valid_person.login == login).one_or_none()
         person.code = code
         person.status = 2
         session.commit()
@@ -20,28 +19,40 @@ class UserRepository(BaseRepository):
         """"Inserts login with discord name into database"""
         session.add(Permit(login=login, discord_ID=discord_id))
 
-        person = session.query(Valid_person).\
-            filter(Valid_person.login == login).one_or_none()
+        person = session.query(Valid_person).filter(Valid_person.login == login).one_or_none()
         person.status = 0
 
         session.commit()
 
     def has_unverified_login(self, login: str):
         """"Checks if there's a login """
-        query = session.query(Valid_person).\
-            filter(Valid_person.login == login, Valid_person.status == 1).\
-            one_or_none()
+        query = (
+            session.query(Valid_person)
+            .filter(Valid_person.login == login, Valid_person.status == 1)
+            .one_or_none()
+        )
         return True if query is not None else False
 
     def get_user(self, login: str, status: int = 2):
         """"Finds login from database"""
-        user = session.query(Valid_person).\
-            filter(Valid_person.login == login,
-                   Valid_person.status == status).\
-            one_or_none()
+        user = (
+            session.query(Valid_person)
+            .filter(Valid_person.login == login, Valid_person.status == status)
+            .one_or_none()
+        )
         return user
 
+    def get_fit_user_by_id(self, discord_ID):
+        """Returns user specified by discord ID with FIT year"""
+        return (
+            session.query(Permit, Valid_person)
+            .filter(Permit.discord_ID == discord_ID)
+            .outerjoin(Permit.login == Valid_person.login)
+            .filter(Valid_person.year.contains("FIT"))
+            .one_or_none()
+        )
+
     def add_user(self, login: str, year: str, status: int = 1):
-        """"Finds login from database"""
+        """Add user to database"""
         session.add(Valid_person(login=login, year=year, status=status))
         session.commit()
