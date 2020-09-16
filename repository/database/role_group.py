052ab8c9400d2d21dccccbd74fb750131bf4c0ed
toaster192 <toastercz@gmail.com>
2020-09-16 12:00:26 +0200

    Role group database changes

--- repository/database/role_group.py
+++ repository/database/role_group.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.types import ARRAY
+from sqlalchemy import Column, String
+from sqlalchemy.types import PickleType
+from sqlalchemy.ext.mutable import MutableList
 from repository.database import database
 
 
@@ -7,5 +8,5 @@ class RoleGroup(database.base):
     __tablename__ = 'bot_role_group'
 
     name = Column(String, primary_key=True)
-    role_ids = Column(ARRAY(Integer))
-    channel_ids = Column(ARRAY(Integer))
+    role_ids = Column(MutableList.as_mutable(PickleType))
+    channel_ids = Column(MutableList.as_mutable(PickleType))
