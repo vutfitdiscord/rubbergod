e67aae2076b9387e09e51dd146382ce224c15181
wereii <8504482+wereii@users.noreply.github.com>
2020-10-01 22:42:54 +0200

    Formatting, hug ranks and some tidbits

--- cogs/hugs/hugs.py
+++ cogs/hugs/hugs.py
@@ -20,7 +20,7 @@ class Hugs(commands.Cog):
         self.bot = bot
         self.hugs_repo = HugsRepository()
 
-    @commands.cooldown(rate=2, per=30.0, type=commands.BucketType.user)
+    @commands.cooldown(rate=2, per=60.0, type=commands.BucketType.user)
     @commands.command()
     async def hugboard(self, ctx: commands.Context):
         """
@@ -31,7 +31,7 @@ class Hugs(commands.Cog):
 
         await menu.start(ctx)
 
-    @commands.cooldown(rate=2, per=30.0, type=commands.BucketType.user)
+    @commands.cooldown(rate=2, per=60.0, type=commands.BucketType.user)
     @commands.command()
     async def huggers(self, ctx: commands.Context):
         """
@@ -42,7 +42,7 @@ class Hugs(commands.Cog):
 
         await menu.start(ctx)
 
-    @commands.cooldown(rate=2, per=30.0, type=commands.BucketType.user)
+    @commands.cooldown(rate=2, per=60.0, type=commands.BucketType.user)
     @commands.command()
     async def hugged(self, ctx: commands.Context):
         """
@@ -53,24 +53,42 @@ class Hugs(commands.Cog):
 
         await menu.start(ctx)
 
-    @commands.cooldown(rate=5, per=30.0, type=commands.BucketType.user)
+    @commands.cooldown(rate=5, per=60.0, type=commands.BucketType.user)
     @commands.command()
     async def hugs(self, ctx: commands.Context):
         """
         Get your lovely hug stats.
         """
         async with ctx.typing():
-            embed = discord.Embed(title="Your Lovely Hug Stats")
+            stats = self.hugs_repo.get_members_stats(ctx.author.id)
+            positions = self.hugs_repo.get_member_position(stats)
+            avg_position = int((positions[0] + positions[1]) // 2)
+
+            embed = discord.Embed(
+                title="Your Lovely Hug Stats <:peepoHugger:761293739041620038>",
+                description=" | ".join(
+                    (
+                        "**Ranks**",
+                        f"Given: **{positions[0]}.**",
+                        f"Received: **{positions[1]}.**",
+                        f"Avg: **{avg_position}.**",
+                    )
+                ),
+            )
             embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
 
-            stats = self.hugs_repo.get_members_stats(ctx.author.id)
+            if ctx.guild and ctx.guild.id == config.guild_id:
+                given_emoji = "<:peepohugs:761297281743716373>"
+                recv_emoji = "<:huggers:602823825880514561>"
+            else:
+                given_emoji = recv_emoji = ":people_hugging:"
 
-            embed.add_field(name="Given", value=str(stats[0]), inline=False)
-            embed.add_field(name="Received", value=str(stats[1]), inline=False)
+            embed.add_field(name=f"{given_emoji} Given", value=str(stats.given))
+            embed.add_field(name=f"{recv_emoji} Received", value=str(stats.received))
 
         await ctx.send(embed=embed)
 
-    @commands.cooldown(rate=5, per=20.0, type=commands.BucketType.user)
+    @commands.cooldown(rate=5, per=60.0, type=commands.BucketType.user)
     @commands.command()
     async def hug(self, ctx: commands.Context, user: discord.Member = None, intensity: int = 0):
         """Because everyone likes hugs"""
@@ -86,10 +104,11 @@ class Hugs(commands.Cog):
                 self.hugs_repo.do_hug(giver_id=ctx.author.id, receiver_id=user.id)
 
             user_str = discord.utils.escape_markdown(user.display_name).replace("@", "@ ")
-            if 0 <= intensity < len(emojis):
-                await ctx.send(emojis[intensity] + f" **{user_str}**")
-            else:
-                await ctx.send(choice(emojis) + f" **{user_str}**")
+
+        if 0 <= intensity < len(emojis):
+            await ctx.send(emojis[intensity] + f" **{user_str}**")
+        else:
+            await ctx.send(choice(emojis) + f" **{user_str}**")
 
     @hug.error
     async def hug_error(self, ctx, error):
--- repository/hugs_repo.py
+++ repository/hugs_repo.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from typing import Optional, Tuple, Union
 
 import discord
@@ -8,6 +9,8 @@ from repository.database.hugs import HugsTable
 
 Member = Union[discord.Member, discord.User]
 
+HugStats = namedtuple("Hugstats", ("given", "received"))
+
 
 class HugsRepository(BaseRepository):
     def __init__(self):
@@ -17,24 +20,43 @@ class HugsRepository(BaseRepository):
     def _get_member(member_id) -> Optional[HugsTable]:
         return session.query(HugsTable).filter(HugsTable.member_id == member_id).one_or_none()
 
-    def get_members_stats(self, member_id) -> Tuple[int, int]:
+    def get_members_stats(self, member_id) -> HugStats:
         """
         :param member_id: Member's discord id
         :return: Tuple of [Given, Received] hugs, both zero if member not found
         """
         hugs = self._get_member(member_id)
-        return (hugs.given, hugs.received) if hugs else (0, 0)
+        return HugStats(hugs.given, hugs.received) if hugs else HugStats(0, 0)
+
+    @staticmethod
+    def _get_count(order_by=None):
+        # this should be cached for some time
+        return session.query(HugsTable).order_by(order_by).count()
+
+    def get_member_position(self, member_stats: HugStats) -> Tuple[int, int]:
+        if member_stats.given > 0:
+            give_position = 1 + session.query(HugsTable).filter(HugsTable.given > member_stats.given).count()
+        else:
+            # zero means you are last
+            give_position = self._get_count(order_by=HugsTable.given)
+
+        if member_stats.received > 0:
+            recv_position = 1 + session.query(HugsTable).filter(HugsTable.received > member_stats.received).count()
+        else:
+            recv_position = self._get_count(order_by=HugsTable.received)
+
+        return give_position, recv_position
 
     @staticmethod
     def get_top_all_query():
         return session.query(HugsTable).order_by(HugsTable.given.desc(), HugsTable.received.desc())
 
     @staticmethod
-    def get_top_givers_query(limit=10, offset=0):
+    def get_top_givers_query():
         return session.query(HugsTable).order_by(HugsTable.given.desc())
 
     @staticmethod
-    def get_top_receivers_query(limit=10, offset=0):
+    def get_top_receivers_query():
         return session.query(HugsTable).order_by(HugsTable.received.desc())
 
     def do_hug(self, giver_id: int = None, receiver_id: int = None):
