02565375eb8a4660d9d804721419f802cc40221c
wereii <8504482+wereii@users.noreply.github.com>
2020-10-04 17:52:04 +0200

    Another custom classes for menu - solves some silenced errors with buttons; Additionally handle author footer in LeaderboardPageSource.

--- features/menus/source.py
+++ features/menus/source.py
@@ -18,6 +18,7 @@ from discord.ext.menus import PageSource, Menu
 from sqlalchemy.orm.query import Query
 from sqlalchemy.schema import Table
 
+import utils
 from config import app_config
 
 config = app_config.Config()
@@ -48,7 +49,7 @@ class DatabaseIteratorPageSource(PageSource):
         self._query = query
 
         self.per_page = per_page
-        self.current_page = -1  # let's start from 0, see .get_page
+        self.current_page = 0
 
     @cached_property
     def _get_max_pages(self):
@@ -63,11 +64,11 @@ class DatabaseIteratorPageSource(PageSource):
 
     async def get_page(self, page_number) -> DatabasePage:
         # result of this is passed into format_page(..., page) arg
-        self.current_page += 1
+        self.current_page = page_number
         return self._query.limit(self.per_page).offset(self.current_page * self.per_page).all()
 
     def is_paginating(self) -> bool:
-        return self.current_page < self.get_max_pages()
+        return (self.current_page + 1) < self.get_max_pages()
 
     def format_page(self, menu: Menu, page: DatabasePage) -> Union[str, discord.Embed, dict]:
         raise NotImplemented
@@ -79,39 +80,52 @@ base_leaderboard_format_str = "_{position}._ - **{member_name}**:"
 class LeaderboardPageSource(DatabaseIteratorPageSource):
     base_embed = None
 
-    def __init__(self, row_formatter: Union[str, Callable], base_embed: discord.Embed = None, *args, **kwargs):
+    def __init__(
+            self,
+            row_formatter: Union[str, Callable],
+            query: Query,
+            per_page=10,
+            base_embed: discord.Embed = None,
+    ):
         """
-        Initialize this page source, ``query`` and ``row_formatter`` are required.
-
+        Initialize this page source.
 
         row_formatter: :class:`str` or :class:`Callable`
-            These are always supplied as keywords either into str.format or call of the formatter:
+            These are always supplied as keywords either into
+            ``str.format`` or call of the ``formatter``:
                 - ``position`` :class:`int`
                 - ``member_name`` :class:`str`
                 - ``entry`` :class:`Table`
 
         base_embed: :class:`discord.Embed`, defaults to empty Embed
             The embed that will be used as a foundation for the message,
-            it's ``.description`` will be used for pagination.
+            it's ``.description`` and ``.footer`` will be used
+            for the pagination purposes.
 
         Warning: The table entry returned by the underlying ``Query`` has to contain
             ``member_id/_ID`` column.
         """
         self.row_formatter: Union[str, Callable] = row_formatter
         self.base_embed = base_embed if base_embed else discord.Embed()
-        super().__init__(*args, **kwargs)
+
+        super().__init__(query=query, per_page=per_page)
 
     @staticmethod
     async def _get_member_name(ctx: Context, member_id: Union[str, int]) -> str:
         member = ctx.guild.get_member(member_id)
-        return discord.utils.escape_markdown(member.name)
+        if not member:
+            return "_User left_"
+        return discord.utils.escape_markdown(str(member))
 
     async def _format_row(self, entry: Table, position: int, ctx: Context) -> str:
         """
+        Applies current query results onto self.row_formatter.
+        The result's member_id/ID attribute value is converted into string and available
+        as member_name keyword for the formatter.
 
         :raises :class:`AssertionError`:
-            If neither ``member_id`` nor ``member_ID`` is found in entry's columns
-        :returns: Formatted result of `self.row_format_str`.
+            If neither ``member_id`` nor ``member_ID`` column is found in data returned from query.
+        :returns: Formatted string, result of calling/applying `self.row_formatter`.
         """
 
         # try to guess member_id key
@@ -133,5 +147,10 @@ class LeaderboardPageSource(DatabaseIteratorPageSource):
             lines.append(await self._format_row(entry, (self.current_page * self.per_page) + i + 1, menu.ctx))
 
         self.base_embed.description = "\n" + "\n".join(lines)
+
+        # possibility to optimize, author should be set only once
+        utils.add_author_footer(
+            self.base_embed, menu.ctx, additional_text=(f"{self.current_page + 1}/{self.get_max_pages()} pages.",)
+        )
 
         return self.base_embed
