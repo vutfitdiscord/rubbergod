01688c0c6c4f7c5664e7c213d9920fea84051b6b
wereii <8504482+wereii@users.noreply.github.com>
2020-12-08 00:12:01 +0100

    Finalized basic menus

---
+++ features/menus/base.py
@@ -0,0 +1,65 @@
+import asyncio
+from typing import Union
+
+from discord import User, Member
+from discord.ext.menus import MenuPages
+
+
+class MenuInnerException(Exception):
+    __cause__ = "Inner task raised exception."
+
+
+class BlockingPagedMenu(MenuPages):
+    _inner_exception = None
+    inner_error_ev = asyncio.Event()
+
+    """
+    Handles some very much unexpected things with discord.ext.menu's.
+
+        - Setting an emoji on button that discord does not recognise would
+          make pagination silently fail. This class hacks it so these
+          exceptions are raised in start() call and thus propagated
+          into command caller.
+    """
+
+    async def start(self, ctx, *, channel=None, wait=False):
+        """
+        Starts the pagination by sending the first page and then
+        blocks until the pagination session finishes.
+
+        :raises Exception: Propagates errors from inner tasks.
+        """
+        await super().start(ctx, channel=channel, wait=wait)
+
+        def handle_task_exc(inner_task: asyncio.Future):
+            try:
+                exc = inner_task.exception()
+                if exc:
+                    self._inner_exception = exc
+                    self.inner_error_ev.set()
+            except asyncio.CancelledError:
+                pass
+
+        for task in self._Menu__tasks:  # noqa
+            task.add_done_callback(handle_task_exc)
+
+        await self._wait_finish_or_exc()
+
+        if self._inner_exception:
+            inner_exc = MenuInnerException(self._inner_exception)
+            # professional exception juggling
+
+            try:
+                self.stop()
+            except Exception as stop_exc:
+                raise stop_exc from inner_exc
+            else:
+                raise inner_exc
+        else:
+            self.stop()
+
+    async def _wait_finish_or_exc(self):
+        await asyncio.wait([self.inner_error_ev.wait(), self._event.wait()], return_when=asyncio.FIRST_COMPLETED)
+
+
+MemberLike = Union[User, Member]
--- features/menus/menu.py
+++ features/menus/menu.py
@@ -1,64 +1,20 @@
 import asyncio
-from typing import Union
 
 import discord
-from discord import Message, User, Member
+from discord import Message
 from discord.ext.commands import Bot, Context
-from discord.ext.menus import button, Last, MenuPages, First
+from discord.ext.menus import button, Last, First
 
 from config.app_config import Config
+from features.menus.base import BlockingPagedMenu, MemberLike
 
 config = Config()
 
-MemberLike = Union[User, Member]
 
-
-class CustomPagedMenu(MenuPages):
+class AuthorOnlyPagedMenu(BlockingPagedMenu, inherit_buttons=False):
     """
-    Handles some very much unexpected things with discord.ext.menu's.
-
-        - Setting an emoji on button that discord does not recognise would
-          make pagination silently fail. This class hacks it so these
-          exceptions are raised in start() call and thus propagated
-          into command caller.
+    Menu that allows react control only for the message owner or admins.
     """
-
-    async def start(self, ctx, *, channel=None, wait=False):
-        """
-        Starts the pagination by sending the first page and then
-        blocks until the pagination session finishes.
-
-        :raises Exception: Propagates errors from inner tasks.
-        """
-        await super().start(ctx, channel=channel, wait=wait)
-        _inner_exceptions = list()
-
-        def task_exc_cleaner(fut: asyncio.Future):
-            exc = fut.exception()
-            if exc:
-                _inner_exceptions.append(exc)
-
-        for task in self._Menu__tasks:  # noqa
-            task.add_done_callback(task_exc_cleaner)
-
-        while True:
-            # need to check at the end
-            # the menu's main loop might be already finished but we
-            # need to check errors at least once more because that
-            # will be the moment when all task's done_callbacks should be finished too
-            for exc in _inner_exceptions:
-                raise Exception("Inner task raised exception.") from exc
-
-            if self._event.is_set():
-                return
-            await asyncio.sleep(0.05)
-
-
-class AuthorOnlyPagedMenu(CustomPagedMenu, inherit_buttons=False):
-    """
-    MenuPage that allows react control only for the message owner or admins.
-    """
-
     bot: Bot
     ctx: Context
     message: Message
@@ -68,66 +24,78 @@ class AuthorOnlyPagedMenu(CustomPagedMenu, inherit_buttons=False):
         super().__init__(source, **kwargs)
 
     async def start(self, ctx: Context, *, channel=None, wait=False):
+        assert (hasattr(ctx, "author")
+                and ctx.author), "ctx.author does not exists or is falsy, that's unexpected"
+
         self.author = ctx.author
         await super().start(ctx, channel=channel, wait=wait)
 
     @staticmethod
     def _is_add_event(payload: discord.RawReactionActionEvent) -> bool:
-        return payload.event_type == "REACTION_ADD"
+        return hasattr(payload, "event_type") and payload.event_type == "REACTION_ADD"
 
     def _is_author_or_admin(self, payload: discord.RawReactionActionEvent) -> bool:
-        return payload.user_id == self.author.id or payload.user_id in config.admin_ids
+        return hasattr(payload, "user_id") and (
+                payload.user_id == self.author.id or payload.user_id in config.admin_ids
+        )
 
     async def _try_remove_reaction(self, payload: discord.RawReactionActionEvent):
         if self.ctx.message.guild and self.ctx.message.guild.me.guild_permissions.manage_messages:
             # try removing the reaction afterwards
             try:
                 await self.message.remove_reaction(payload.emoji, payload.member)
-            except discord.DiscordException:
+            except (discord.DiscordException, AttributeError):
                 pass
 
-    async def _main_checks(self, payload: discord.RawReactionActionEvent) -> bool:
+    async def _checks(self, payload: discord.RawReactionActionEvent, remove_react_after=True) -> bool:
+        """Checks it's reaction ADD event and message ownership.
+        Additionally allows for the users react removal.
+        """
         if self._is_add_event(payload):
-            asyncio.ensure_future(self._try_remove_reaction(payload), loop=self.bot.loop)
+            if remove_react_after:
+                asyncio.ensure_future(self._try_remove_reaction(payload), loop=self.bot.loop)
             if self._is_author_or_admin(payload):
                 return True
         return False
 
-    def _skip_fast_forward_first(self):
-        return self.current_page <= 1
+    def _check_fast_forward(self):
+        return self._source.get_max_pages() < 3
 
-    @button('\N{BLACK LEFT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}\ufe0f',
-            position=First(0), skip_if=_skip_fast_forward_first)
-    async def go_to_first_page(self, payload):
-        """go to the first page"""
-        if await self._main_checks(payload):
-            await self.show_page(0)
-
-    @button('\N{BLACK LEFT-POINTING TRIANGLE}\ufe0f', position=First(1))
+    @button("\N{BLACK LEFT-POINTING TRIANGLE}\ufe0f", position=First(1))
     async def go_to_previous_page(self, payload):
         """go to the previous page"""
-        if await self._main_checks(payload):
+        if await self._checks(payload):
             await self.show_checked_page(self.current_page - 1)
 
-    @button('\N{BLACK RIGHT-POINTING TRIANGLE}\ufe0f', position=Last(0))
+    @button("\N{BLACK RIGHT-POINTING TRIANGLE}\ufe0f", position=Last(0))
     async def go_to_next_page(self, payload):
         """go to the next page"""
-        if await self._main_checks(payload):
+        if await self._checks(payload):
             await self.show_checked_page(self.current_page + 1)
 
-    def _skip_fast_forward_last(self):
-        return self._source.get_max_pages() <= 2
+    @button(
+        "\N{BLACK LEFT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}\ufe0f",
+        position=First(0),
+        skip_if=_check_fast_forward,
+    )
+    async def go_to_first_page(self, payload):
+        """go to the first page"""
+        if await self._checks(payload):
+            await self.show_page(0)
 
-    @button('\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}\ufe0f',
-            position=Last(1), skip_if=_skip_fast_forward_last)
+    @button(
+        "\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}\ufe0f",
+        position=Last(1),
+        skip_if=_check_fast_forward,
+    )
     async def go_to_last_page(self, payload):
         """go to the last page"""
         # The call here is safe because it's guarded by skip_if
-        if await self._main_checks(payload):
+        if await self._checks(payload):
             await self.show_page(self._source.get_max_pages() - 1)
 
-    @button('\N{BLACK SQUARE FOR STOP}\ufe0f', position=Last(2))
+    @button("\N{BLACK SQUARE FOR STOP}\ufe0f", position=Last(2))
     async def stop_pages(self, payload):
         """stops the pagination session."""
-        if await self._main_checks(payload):
+        if await self._checks(payload):
             self.stop()
