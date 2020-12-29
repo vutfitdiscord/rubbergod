5bd29338db0c4dbcfdcb67dc2d2090dd5bb091d5
wereii <8504482+wereii@users.noreply.github.com>
2020-12-29 20:36:03 +0100

    Fixed inconsistencies in hugs
    - Nicknames were ignored
    - Invalid user names with ?hugs sent wrong response
    - Reflect stalking in ?hugs title
    - All emojis now get fetched from the base guild

--- cogs/hugs/hugs.py
+++ cogs/hugs/hugs.py
@@ -1,3 +1,4 @@
+from functools import partial, lru_cache
 from random import choice
 
 import discord
@@ -22,6 +23,10 @@ class Hugs(commands.Cog):
         self.hugs_repo = HugsRepository()
         self.check = room_check.RoomCheck(bot)
 
+    @lru_cache(5)
+    def get_default_emoji(self, emoji: str):
+        return utils.get_emoji(self.bot.get_guild(config.guild_id), emoji)
+
     @commands.cooldown(rate=2, per=60.0, type=commands.BucketType.user)
     @commands.command()
     async def hugboard(self, ctx: commands.Context):
@@ -31,7 +36,9 @@ class Hugs(commands.Cog):
         async with ctx.typing():
             menu = get_hugboard_menu(
                 base_embed=discord.Embed(
-                    title="{0} HUGBOARD {0}".format(utils.get_emoji(ctx.message.guild, "peepoHugger"))
+                    title="{0} HUGBOARD {0}".format(
+                        self.get_default_emoji("peepoHugger") or ""
+                    )
                 )
             )
 
@@ -47,7 +54,9 @@ class Hugs(commands.Cog):
         async with ctx.typing():
             menu = get_top_huggers_menu(
                 base_embed=discord.Embed(
-                    title="{0} TOP HUGGERS {0} ".format(utils.get_emoji(ctx.message.guild, "peepoHugger"))
+                    title="{0} TOP HUGGERS {0} ".format(
+                        self.get_default_emoji("peepoHugger") or ""
+                    )
                 )
             )
         await self.check.botroom_check(ctx.message)
@@ -62,7 +71,9 @@ class Hugs(commands.Cog):
         async with ctx.typing():
             menu = get_top_hugged_menu(
                 base_embed=discord.Embed(
-                    title="{0} TOP HUGGED {0} ".format(utils.get_emoji(ctx.message.guild, "peepoHugger"))
+                    title="{0} TOP HUGGED {0} ".format(
+                        self.get_default_emoji("peepoHugger") or ""
+                    )
                 )
             )
 
@@ -71,12 +82,17 @@ class Hugs(commands.Cog):
 
     @commands.cooldown(rate=5, per=60.0, type=commands.BucketType.user)
     @commands.command()
-    async def hugs(self, ctx: commands.Context, user: discord.User = None):
+    async def hugs(self, ctx: commands.Context, user: discord.Member = None):
         """
         Get your lovely hug stats.
         """
-        if user is None:
+        if user is None or user == ctx.author:
             user = ctx.author
+            user_str = utils.get_username(user)
+            title = "{0} Your Lovely Hug Stats {0}"
+        else:
+            user_str = utils.get_username(user)
+            title = f"{{0}} {user_str}'s Lovely Hug Stats {{0}}"
 
         async with ctx.typing():
             stats = self.hugs_repo.get_members_stats(user.id)
@@ -84,8 +100,8 @@ class Hugs(commands.Cog):
             avg_position = int((positions[0] + positions[1]) // 2)
 
             embed = discord.Embed(
-                title="{0} Your Lovely Hug Stats {0}".format(
-                    utils.get_emoji(ctx.message.guild, "peepoHugger")
+                title=title.format(
+                    self.get_default_emoji("peepoHugger") or ""
                 ),
                 description=" | ".join(
                     (
@@ -97,14 +113,11 @@ class Hugs(commands.Cog):
                 ),
             )
 
-            embed.set_author(name=user.name, icon_url=user.avatar_url)
+            embed.set_author(name=user_str, icon_url=user.avatar_url)
             utils.add_author_footer(embed, ctx.author)
 
-            if ctx.guild and ctx.guild.id == config.guild_id:
-                given_emoji = utils.get_emoji(ctx.message.guild, "peepohugs")
-                recv_emoji = utils.get_emoji(ctx.message.guild, "huggers")
-            else:
-                given_emoji = recv_emoji = ":people_hugging:"
+            given_emoji = self.get_default_emoji("peepohugs") or ""
+            recv_emoji = self.get_default_emoji("huggers") or ""
 
             embed.add_field(name=f"{given_emoji} Given", value=str(stats.given))
             embed.add_field(name=f"{recv_emoji} Received", value=str(stats.received))
@@ -114,12 +127,12 @@ class Hugs(commands.Cog):
 
     @commands.cooldown(rate=5, per=60.0, type=commands.BucketType.user)
     @commands.command()
-    async def hug(self, ctx: commands.Context, user: discord.User = None, intensity: int = 0):
+    async def hug(self, ctx: commands.Context, user: discord.Member = None, intensity: int = 0):
         """Because everyone likes hugs"""
         if user is None:
             user = ctx.author
         elif user.bot:
-            await ctx.send(utils.get_emoji(ctx.message.guild, "huggers"))
+            await ctx.send(self.get_default_emoji("huggers") or ":people_hugging:")
             return
 
         async with ctx.typing():
@@ -127,13 +140,14 @@ class Hugs(commands.Cog):
             if user != ctx.author:
                 self.hugs_repo.do_hug(giver_id=ctx.author.id, receiver_id=user.id)
 
-            user_str = discord.utils.escape_markdown(user.display_name).replace("@", "@ ")
+            user_str = utils.get_username(user)
 
         if 0 <= intensity < len(emojis):
             await ctx.send(f"{emojis[intensity]} **{user_str}**")
         else:
             await ctx.send(f"{choice(emojis)} **{user_str}**")
 
+    @hugs.error
     @hug.error
     async def hug_error(self, ctx, error):
         if isinstance(error, commands.BadArgument):
