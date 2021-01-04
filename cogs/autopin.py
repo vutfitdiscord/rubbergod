feb4de55781727389739e78e27190841aba6f388
Peter Dragun <pdragun@redhat.com>
2021-01-04 19:47:46 +0100

    mention author of reaction if pin is not possible

--- cogs/autopin.py
+++ cogs/autopin.py
@@ -134,7 +134,7 @@ class AutoPin(commands.Cog):
             ):
                 pin_count = await channel.pins()
                 if len(pin_count) == 50:
-                    await channel.send(Messages.autopin_max_pins_error)
+                    await channel.send(f"{ctx['member'].mention} {Messages.autopin_max_pins_error}")
                     return
                 users = await reaction.users().flatten()
                 await self.log(message, users)
