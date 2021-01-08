5f4af4760efa46f2890d8e36906c4d3ce3e8e7b7
Peter Dragun <pdragun@redhat.com>
2021-01-08 02:27:14 +0100

    handle exceptions in cog load

--- cogs/system.py
+++ cogs/system.py
@@ -9,9 +9,7 @@ class System(commands.Cog):
         self.bot = bot
         self.git = Git()
 
-        self.unloadable_cogs = [
-            'system'
-        ]
+        self.unloadable_cogs = ["system"]
 
     @commands.group(pass_context=True)
     async def git(self, ctx: commands.Context):
@@ -20,37 +18,59 @@ class System(commands.Cog):
     @git.command()
     @commands.check(utils.is_bot_admin)
     async def pull(self, ctx: commands.Context):
-        message: Message = await ctx.send('Pulling')
+        message: Message = await ctx.send("Pulling")
 
         pull_result = await self.git.pull(ctx)
         pull_parts = utils.cut_string(pull_result, 1900)
 
-        await message.edit(content=f'```{pull_parts[0]}```')
+        await message.edit(content=f"```{pull_parts[0]}```")
 
         for part in pull_parts[1:]:
-            await ctx.send(f'```{part}```')
+            await ctx.send(f"```{part}```")
 
     @commands.command()
     @commands.check(utils.is_bot_admin)
     async def load(self, ctx: commands.Context, extension: str):
-        self.bot.load_extension(f'cogs.{extension}')
-        await ctx.send(utils.fill_message('cog_loaded', cog=extension))
+        """Load cog"""
+        try:
+            self.bot.load_extension(f"cogs.{extension}")
+            await ctx.send(utils.fill_message("cog_loaded", cog=extension))
+        except Exception as e:
+            await ctx.send(f"Loading error\n`{e}`")
 
     @commands.command()
     @commands.check(utils.is_bot_admin)
     async def unload(self, ctx: commands.Context, extension: str):
+        """Unload cog"""
         if extension in self.unloadable_cogs:
-            await ctx.send(utils.fill_message('cog_cannot_be_unloadable', cog=extension))
+            await ctx.send(utils.fill_message("cog_cannot_be_unloadable", cog=extension))
             return
 
-        self.bot.unload_extension(f'cogs.{extension}')
-        await ctx.send(utils.fill_message('cog_unloaded', cog=extension))
+        try:
+            self.bot.unload_extension(f"cogs.{extension}")
+            await ctx.send(utils.fill_message("cog_unloaded", cog=extension))
+        except Exception as e:
+            await ctx.send(f"Unloading error\n`{e}`")
 
     @commands.command()
     @commands.check(utils.is_bot_admin)
     async def reload(self, ctx: commands.Context, extension: str):
-        self.bot.reload_extension(f'cogs.{extension}')
-        await ctx.send(utils.fill_message('cog_reloaded', cog=extension))
+        """Reload cog"""
+        try:
+            self.bot.reload_extension(f"cogs.{extension}")
+            await ctx.send(utils.fill_message("cog_reloaded", cog=extension))
+        except Exception as e:
+            await ctx.send(f"Reloading error\n`{e}`")
+
+    @commands.command(aliases=["extensions"])
+    @commands.check(utils.is_bot_admin)
+    async def cogs(self, ctx: commands.Context):
+        """Return loaded cogs"""
+        cogs = self.bot.cogs
+        cog_names = []
+        for cog in cogs:
+            cog_names.append(str(cog))
+        await ctx.send(cog_names)
 
     @pull.error
     @load.error
@@ -58,14 +78,14 @@ class System(commands.Cog):
     @reload.error
     async def on_command_error(self, ctx: commands.Context, error):
         if isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send('Missing argument.')
+            await ctx.send("Missing argument.")
         if isinstance(error, commands.errors.CheckFailure):
             await ctx.send(utils.fill_message("insufficient_rights", user=ctx.author.id))
         if isinstance(error, commands.errors.CommandInvokeError):
             if isinstance(error.__cause__, commands.errors.ExtensionAlreadyLoaded):
-                await ctx.send(utils.fill_message('cog_is_loaded', cog=error.__cause__.name))
+                await ctx.send(utils.fill_message("cog_is_loaded", cog=error.__cause__.name))
             elif isinstance(error.__cause__, commands.errors.ExtensionNotLoaded):
-                await ctx.send(utils.fill_message('cog_is_unloaded', cog=error.__cause__.name))
+                await ctx.send(utils.fill_message("cog_is_unloaded", cog=error.__cause__.name))
 
 
 def setup(bot):
