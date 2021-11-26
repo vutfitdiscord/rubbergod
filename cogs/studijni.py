9dbbf1c2636f21acf993585c2ce2dce1b3ea02b2
Peter Dragun <pdragun@redhat.com>
2021-11-26 13:03:35 +0100

    make studijni command more resistant to changes on web

--- cogs/studijni.py
+++ cogs/studijni.py
@@ -15,12 +15,23 @@ class Studijni(commands.Cog):
         session = requests.session()
         result = session.get(link)
         xDoc2 = etree.fromstring(result.text, htmlparser)
-        hours_div = xDoc2.xpath("//p[b[contains(text(),'Úřední hodiny')]]//following-sibling::div/p")
-        hours = etree.tostring(hours_div[0], encoding=str, method="text")
-        warning = etree.tostring(hours_div[1], encoding=str, method="text").split(':', 1)
-        embed = discord.Embed(title="C109 Studijní oddělení", url=link)
-        embed.add_field(name="Úřední hodiny", value=hours, inline=False)
-        embed.add_field(name=warning[0], value=warning[1], inline=False)
+        hours_div = xDoc2.xpath("//*[b[contains(text(),'Úřední hodiny')]]//following-sibling::div")            
+        embed = discord.Embed(title=Messages.studijni_title, url=link)
+        if hours_div:
+            hours = etree.tostring(hours_div[0], encoding=str, method="text")
+            additional_info = xDoc2.xpath("//main//section/p")
+            if additional_info:
+                info = etree.tostring(additional_info[0], encoding=str, method="text").split(':', 1)
+                if len(info) > 1: 
+                    embed.add_field(name=info[0], value=info[1], inline=False)
+        else:
+            hours_div = xDoc2.xpath("//main//section")
+            if len(hours_div):
+                hours = ''.join(hours_div[0].itertext())
+                hours = hours.strip()
+            else:
+                hours = Messages.studijni_web_error
+        embed.add_field(name=Messages.studijni_office_hours, value=hours, inline=False)
         add_author_footer(embed, ctx.message.author)
         await ctx.send(embed=embed)
 
