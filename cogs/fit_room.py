010cf8ecb65a7b930b3e1785ab4ace2f71d71b99
Martin Dousa <martindousa186@gmail.com>
2022-01-05 15:02:11 +0100

    better?

--- cogs/fit_room.py
+++ cogs/fit_room.py
@@ -24,29 +24,29 @@ class FitRoom(commands.Cog):
         async with ctx.typing():
             try:
                 soup = BeautifulSoup(r.content, 'html.parser')
-                main_body_ = soup.find("main", {"id": "main"})
-                floor_list_ = main_body_.find("ul", {"class": "pagination__list"})
-                active_floor_ = floor_list_.find("a", {"aria-current": "page"})
-                image_ = main_body_.find("svg")
-                overlay_ = image_.find("g", {"id": "layer3"})
-                cursor_ = overlay_.find("polygon",
-                                        {"style": "fill:red;stroke:none;pointer-events:none"})
+                main_body = soup.find("main", {"id": "main"})
+                floor_list = main_body.find("ul", {"class": "pagination__list"})
+                active_floor = floor_list.find("a", {"aria-current": "page"})
+                image = main_body.find("svg")
+                overlay = image.find("g", {"id": "layer3"})
+                cursor = overlay.find("polygon",
+                                      {"style": "fill:red;stroke:none;pointer-events:none"})
             except:
                 return await ctx.send(Messages.fit_room_parsing_failed)
 
-            if image_ is None or cursor_ is None:
+            if image is None or cursor is None:
                 return await ctx.send(
                     utils.fill_message("fit_room_room_not_on_plan", room=room))
 
             image_bytes = BytesIO()
-            svg2png(bytestring=str(image_).encode("utf-8"), write_to=image_bytes, parent_width=720,
+            svg2png(bytestring=str(image).encode("utf-8"), write_to=image_bytes, parent_width=720,
                     parent_height=1000, background_color="white", dpi=300)
             image_bytes.seek(0)
 
             embed = discord.Embed(title=f"Místnost: {room}", color=discord.Color.dark_blue())
             embed.set_image(url="attachment://plan.png")
             embed.description = f"[Odkaz na plánek]({url})"
-            embed.set_footer(text=str(active_floor_.text))
+            embed.set_footer(text=str(active_floor.text))
             file = discord.File(fp=image_bytes, filename="plan.png")
             await ctx.send(embed=embed, file=file)
 
