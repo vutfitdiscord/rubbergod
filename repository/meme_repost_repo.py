8d3bbe6898e8cccdbd909f91632994646a020e69
Martin Dousa <martindousa186@gmail.com>
2022-01-08 13:45:00 +0100

    better way of resposting with secondary message with additional files

--- repository/meme_repost_repo.py
+++ repository/meme_repost_repo.py
@@ -8,7 +8,7 @@ class MemeRepostRepo:
         return session.query(MemeRepost).filter(MemeRepost.original_message_id == str(message_id)).one_or_none()
 
     @staticmethod
-    def create_repost(original_message_id:int, repost_message_id:int, author_id:int):
-        item = MemeRepost(original_message_id=original_message_id, repost_message_id=repost_message_id, author_id=author_id)
+    def create_repost(original_message_id:int, repost_message_id:int, author_id:int, secondary_repost_message_id:int=None):
+        item = MemeRepost(original_message_id=original_message_id, repost_message_id=repost_message_id, author_id=author_id, secondary_repost_message_id=secondary_repost_message_id)
         session.add(item)
         session.commit()
