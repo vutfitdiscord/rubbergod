ff728eabdfa1ce625d6ecbf0dc93d90b745309f9
Martin Dousa <martindousa186@gmail.com>
2022-01-08 03:41:39 +0100

    Added database table for already reposted messages

---
+++ repository/database/meme_repost.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer
+from repository.database import database
+
+
+class MemeRepost(database.base):
+    __tablename__ = "bot_meme_reposts"
+
+    original_message_id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    author_id = Column(Integer, nullable=False)
+    reposted_message_id = Column(Integer, nullable=False)
