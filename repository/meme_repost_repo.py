c09e853b04007b244fc9f6d5a209592118ee7618
Martin Dousa <martindousa186@gmail.com>
2022-01-08 03:41:39 +0100

    Added database table for already reposted messages

---
+++ repository/meme_repost_repo.py
@@ -0,0 +1,14 @@
+from repository.database import session
+from repository.database.meme_repost import MemeRepost
+from typing import Union
+
+class MemeRepostRepo:
+    @staticmethod
+    def find_repost_by_id(message_id:int) -> Union[MemeRepost, None]:
+        return session.query(MemeRepost).filter(MemeRepost.original_message_id == str(message_id)).one_or_none()
+
+    @staticmethod
+    def create_repost(original_message_id:int, repost_message_id:int, author_id:int):
+        item = MemeRepost(original_message_id=original_message_id, repost_message_id=repost_message_id, author_id=author_id)
+        session.add(item)
+        session.commit()
