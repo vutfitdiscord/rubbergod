56780692dd1bbc8fa72b5a0880979d982f1fb79f
Urumasi <urumasi@email.cz>
2022-02-02 15:28:38 +0100

    Move static function outside, remove debug prints

--- cogs/wordle.py
+++ cogs/wordle.py
@@ -9,10 +9,25 @@ import utils
 
 
 def get_wordle_solution(forward_days: int = 0) -> (int, str):
+    """
+    Returns today's (if forward_days = 0) or other Wordle number and solution
+    If forward_days is not 0, returns solution for Wordle #(today + forward_days)
+    """
     epoch = datetime.date(2021, 6, 19)  # Wordles start counting from this date
     wanted = datetime.date.today() + datetime.timedelta(days=forward_days)
     wordle_number = (wanted - epoch).days
     return wordle_number, config.wordle_solutions[wordle_number]
+
+
+def seconds_until(hour: int, minute: int) -> int:
+    """
+    Gets the amount of seconds until a specific time today
+    If that time has already passed, returns 0
+    """
+    now = datetime.datetime.now()
+    desired = datetime.datetime.now().replace(hour=hour, minute=minute,
+                                              second=0)
+    return max((desired - now).total_seconds(), 0)
 
 
 class Wordle(commands.Cog):
@@ -29,20 +44,12 @@ class Wordle(commands.Cog):
         if self.games_channel is None:
             self.games_channel = await self.bot.fetch_channel(config.games_room)
 
-    @staticmethod
-    def seconds_until(hour: int, minute: int) -> int:
-        now = datetime.datetime.now()
-        desired = datetime.datetime.now().replace(hour=hour, minute=minute,
-                                                  second=0)
-        return max((desired - now).total_seconds(), 0)
-
     @tasks.loop(hours=24)
     async def spoil_job(self):
         # Sleeps until 23:55 (by default) or alter in before_loop
         # Then spoils the next day's wordle every 24 hours
         if self.games_channel is None:
             await self.get_or_fetch_channel()
-            print(f"m {self.games_channel}")
             if self.games_channel is None:
                 return
         solution = get_wordle_solution(1)  # tomorrow
@@ -54,8 +61,8 @@ class Wordle(commands.Cog):
     @spoil_job.before_loop
     async def before_spoil_job(self):
         await self.bot.wait_until_ready()
-        await asyncio.sleep(self.seconds_until(config.wordle_spoil_hour,
-                                               config.wordle_spoil_minute))
+        await asyncio.sleep(
+            seconds_until(config.wordle_spoil_hour, config.wordle_spoil_minute))
 
     @cooldowns.default_cooldown
     @commands.check(utils.helper_plus)
@@ -67,7 +74,6 @@ class Wordle(commands.Cog):
             if self.games_channel is None:
                 return
         solution = get_wordle_solution()
-        print(f'games-{solution[0]}-{solution[1]}')
         await self.games_channel.edit(name=f'games-{solution[0]}-{solution[1]}')
         await self.games_channel.send(
             messages.wordle_spoil_today.format(number=solution[0],
