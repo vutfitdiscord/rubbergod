f50f07f062255a32eba408359db655380e3caaa7
Martin Dousa <martindousa186@gmail.com>
2022-05-03 15:58:58 +0200

    added automatic updating terms in term channels

--- repository/database/exams.py
+++ repository/database/exams.py
@@ -6,4 +6,4 @@ class ExamsTermsMessage(database.base):
     __tablename__ = "exams_terms_message"
 
     message_id = Column(String, primary_key=True)
-    channel_id = Column(String, nullable=False, index=True, unique=True) # One message per channel
+    channel_id = Column(String, nullable=False, index=True)
