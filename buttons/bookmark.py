76b5002872d609f3912e05c8a2841c67bc3d238d
Solumath <solumath99@gmail.com>
2022-06-27 17:29:37 +0200

    reaction listener to reactions cog

--- buttons/bookmark.py
+++ buttons/bookmark.py
@@ -5,6 +5,6 @@ class BookmarkView(disnake.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @disnake.ui.button(label="Delete bookmark", style=disnake.ButtonStyle.danger, custom_id="delete_bookmark")
+    @disnake.ui.button(label="Delete bookmark", style=disnake.ButtonStyle.danger, custom_id="bookmark:delete")
     async def delete_button(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
         pass
