d7fb263dc955170a3df928edff13ebde0db2f3ea
Michal Halabi <m.halabica@gmail.com>
2022-06-29 19:38:54 +0200

    Rewrited GetCode command to interactions

--- cogs/verify.py
+++ cogs/verify.py
@@ -3,6 +3,8 @@ from config import cooldowns
 from features import verification
 from repository import user_repo
 from config.messages import Messages
+import disnake
+from config.app_config import config
 
 
 user_r = user_repo.UserRepository()
@@ -13,15 +15,29 @@ class Verify(commands.Cog):
         self.bot = bot
         self.verification = verification.Verification(bot, user_r)
 
+    def is_valid_channel(ctx: disnake.ApplicationCommandInteraction):
+        return ctx.guild_id is None or ctx.guild_id == config.guild_id
+
     @cooldowns.default_cooldown
     @commands.command(brief=Messages.verify_brief)
     async def verify(self, ctx):
         await self.verification.verify(ctx.message)
 
     @cooldowns.default_cooldown
-    @commands.command(brief=Messages.get_code_brief)
-    async def getcode(self, ctx):
-        await self.verification.send_code(ctx.message)
+    @commands.slash_command(
+        name="getcode", description=Messages.get_code_brief, dm_permission=True
+    )
+    @commands.check(is_valid_channel)
+    async def get_code(self, login: str, inter: disnake.ApplicationCommandInteraction):
+        await self.verification.send_code(login, inter)
+
+    @get_code.error
+    async def on_verification_error(
+        self, inter: disnake.ApplicationCommandInteraction, error
+    ):
+        if (error, commands.CheckFailure):
+            await inter.send(Messages.verify_invalid_channel, ephemeral=True)
+            return True
 
 
 def setup(bot):
--- repository/database/verification.py
+++ repository/database/verification.py
@@ -3,14 +3,14 @@ from repository.database import database
 
 
 class Permit(database.base):
-    __tablename__ = 'bot_permit'
+    __tablename__ = "bot_permit"
 
     login = Column(String, primary_key=True)
     discord_ID = Column(String)
 
 
 class Valid_person(database.base):
-    __tablename__ = 'bot_valid_persons'
+    __tablename__ = "bot_valid_persons"
 
     login = Column(String, primary_key=True)
     name = Column(String)
@@ -18,3 +18,15 @@ class Valid_person(database.base):
     code = Column(String)
     status = Column(Integer, default=1)
     mail = Column(String)
+
+    def get_mail(self, fallback_domain: str) -> str:
+        if self.mail is not None and len(self.mail) > 0:
+            return self.mail
+
+        if len(fallback_domain) == 0:
+            raise Exception(
+                "The user does not have an e-mail address set "
+                + "up and a fallback domain has not been provided."
+            )
+
+        return f"{self.login}@{fallback_domain}"  # fallback
