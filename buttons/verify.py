a0014af41aab0c0d2cd9e623d7b17b9953f9eea8
Michal Halabi <m.halabica@gmail.com>
2022-07-05 17:51:57 +0200

    Fixed formatting

--- buttons/verify.py
+++ buttons/verify.py
@@ -1,6 +1,7 @@
 import disnake
 from features import verification
 
+
 class VerifyView(disnake.ui.View):
     def __init__(self, login: str):
         super().__init__(timeout=None)
@@ -11,9 +12,7 @@ class VerifyView(disnake.ui.View):
         style=disnake.ButtonStyle.success,
         custom_id="verify:set_code",
     )
-    async def code_success(
-        self, button: disnake.ui.Button, inter: disnake.MessageInteraction
-    ):
+    async def code_success(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
         print("Kod dorazil")
 
 
@@ -26,9 +25,6 @@ class VerifyWithResendButtonView(VerifyView):
         style=disnake.ButtonStyle.danger,
         custom_id="verify:missing_code",
     )
-    async def missing_code(
-        self, button: disnake.ui.Button, inter: disnake.MessageInteraction
-    ):
+    async def missing_code(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
         service = verification.Verification(inter.bot)
         await service.resend_code(self.login, inter)
-
