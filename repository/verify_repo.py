d6aa6e0ad701740acd4c64a09896ce118e25fe93
Michal Halabi <m.halabica@gmail.com>
2022-07-14 21:48:01 +0200

    Fixed rule application conditions.

--- repository/verify_repo.py
+++ repository/verify_repo.py
@@ -8,10 +8,18 @@ from typing import List, Union
 
 class VerifyRepository(BaseRepository):
     def exists_rule(self, rule: str) -> bool:
-        return session.query(exists().where(DynamicVerifyRule.id == rule)).scalar()
+        return session.query(
+            exists().where(
+                DynamicVerifyRule.id == rule and DynamicVerifyRule.enabled
+            )
+        ).scalar()
 
     def get_rule(self, rule: str) -> Union[DynamicVerifyRule, None]:
-        return session.query(DynamicVerifyRule).filter(DynamicVerifyRule.id == rule).one_or_none()
+        return (
+            session.query(DynamicVerifyRule)
+            .filter(DynamicVerifyRule.id == rule)
+            .one_or_none()
+        )
 
     def get_rules(self, limit: int) -> List[DynamicVerifyRule]:
         return session.query(DynamicVerifyRule).order_by(asc("id")).limit(limit).all()
