3d6c8f3877c3b748ae97f931f98ef3b776b9faaa
Daniel Fajmon <solumath99@gmail.com>
2022-07-28 13:47:19 +0200

    Update messages.py

--- config/messages.py
+++ config/messages.py
@@ -416,4 +416,4 @@ class Messages:
     base_leaderboard_format_str = "_{position}._ - **{member_name}**:"
 
     bookmark_title = "Záložka na serveru VUT FIT"
-    blocked_bot = "I can't send you messages {author}"
+    blocked_bot = "Nemůžu ti poslat zprávu, protože jsi si mě blocknnul {author}"
