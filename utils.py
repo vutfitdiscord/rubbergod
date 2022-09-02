05fe9bdf68289aca847ffd1debd8d43b084b7d00
Solumath <solumath99@gmail.com>
2022-09-02 13:04:52 +0200

    rewrite channel create, add progress bar

--- utils.py
+++ utils.py
@@ -253,3 +253,20 @@ def split(array, k):
     n = len(array)
     lists = [array[i * (n // k) + min(i, n % k):(i+1) * (n // k) + min(i+1, n % k)] for i in range(k)]
     return lists
+
+
+def create_bar(value, total) -> str:
+    prog_bar_str = ""
+    prog_bar_length = 10
+    percentage = 0
+    if total != 0:
+        percentage = value / total
+        for i in range(prog_bar_length):
+            if round(percentage, 1) <= 1 / prog_bar_length * i:
+                prog_bar_str += "░"
+            else:
+                prog_bar_str += "▓"
+    else:
+        prog_bar_str = "░" * prog_bar_length
+    prog_bar_str = prog_bar_str + f" {round(percentage * 100)}%"
+    return prog_bar_str
