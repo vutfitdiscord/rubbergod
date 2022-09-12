3111a7ce8c75c500fb959e5284a16d970382ea1c
Solumath <solumath99@gmail.com>
2022-09-12 15:54:01 +0200

    use asyncio and aiohttp

--- cogs/name_day.py
+++ cogs/name_day.py
@@ -1,5 +1,6 @@
 import disnake
-import requests
+import aiohttp
+import asyncio
 from datetime import date, time
 from disnake.ext import commands, tasks
 
@@ -13,20 +14,41 @@ class Nameday(commands.Cog):
         self.send_names.start()
 
     async def _svatek(self):
-        url = f"http://svatky.adresa.info/json?date={date.today().strftime('%d%m')}"
-        res = requests.get(url, timeout=10).json()
-        names = []
-        for i in res:
-            names.append(i["name"])
-        return Messages.name_day_cz.format(name=", ".join(names))
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10)) as session:
+            try:
+                url = f"http://svatky.adresa.info/json?date={date.today().strftime('%d%m')}"
+                async with session.get(url) as resp:
+                    res = await resp.json()
+                names = []
+                for i in res:
+                    names.append(i["name"])
+                return Messages.name_day_cz.format(name=", ".join(names))
+            except (asyncio.exceptions.TimeoutError, aiohttp.client_exceptions.ClientConnectorError):
+                return "Website unreachable"
 
     async def _meniny(self):
-        url = f"http://svatky.adresa.info/json?lang=sk&date={date.today().strftime('%d%m')}"
-        res = requests.get(url, timeout=10).json()
-        names = []
-        for i in res:
-            names.append(i["name"])
-        return Messages.name_day_sk.format(name=", ".join(names))
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10)) as session:
+            try:
+                url = f"http://svatky.adresa.info/json?lang=sk&date={date.today().strftime('%d%m')}"
+                async with session.get(url) as resp:
+                    res = await resp.json()
+                names = []
+                for i in res:
+                    names.append(i["name"])
+                return Messages.name_day_sk.format(name=", ".join(names))
+            except (asyncio.exceptions.TimeoutError, aiohttp.client_exceptions.ClientConnectorError):
+                return "Website unreachable"
+
+    async def _narozeniny(self):
+        headers = {"ApiKey": config.grillbot_api_key}
+        async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=10), headers=headers) as session:
+            try:
+                url = "https://grillbot.cloud/api/user/birthday/today"
+                async with session.get(url) as resp:
+                    birthday = await resp.json()
+                    return birthday['message']
+            except (asyncio.exceptions.TimeoutError, aiohttp.client_exceptions.ClientConnectorError):
+                return "Website unreachable"
 
     @commands.slash_command(name="svatek", description=Messages.name_day_cz_brief)
     async def svatek(self, inter: disnake.ApplicationCommandInteraction):
@@ -40,14 +62,14 @@ class Nameday(commands.Cog):
         meniny = await self._meniny()
         await inter.edit_original_message(meniny)
 
-    @tasks.loop(time=time(22, 0, 1))       # UTC time
+    @tasks.loop(time=time(22, 00, 1))       # UTC time
     async def send_names(self):
         svatek = await self._svatek()
         meniny = await self._meniny()
-        headers = {"ApiKey": config.grillbot_api_key}
-        birthday = requests.get("https://grillbot.cloud/api/user/birthday/today", headers=headers)
+        narozeniny = await self._narozeniny()
+
         bot_room = self.bot.get_channel(config.bot_room)
-        await bot_room.send(f"{svatek}\n{meniny}\n{birthday.json()['message']}")
+        await bot_room.send(f"{svatek}\n{meniny}\n{narozeniny}")
 
 
 def setup(bot):
