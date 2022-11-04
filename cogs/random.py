bcec080d824dc0dd6a193b91f11e7aa7ab7269fa
Solumath <solumath99@gmail.com>
2022-11-05 00:35:51 +0100

    get back check for rooms, pick ignores questions
    check for rooms back until reworked for better use for multiple cogs

--- cogs/random.py
+++ cogs/random.py
@@ -4,7 +4,9 @@ import disnake
 from disnake.ext import commands
 
 from config import cooldowns
+from config.app_config import config
 from config.messages import Messages
+import utils
 import shlex
 
 
@@ -17,10 +19,14 @@ class Random(commands.Cog):
     async def pick(
         self,
         inter: disnake.ApplicationCommandInteraction,
-        args: str = commands.Param(max_length=1900)
+        args: str = commands.Param(max_length=1900, description=Messages.random_pick_format)
     ):
         """"Pick an option"""
         args = shlex.split(args)
+        for i, arg in enumerate(args):
+            if "?" in arg:
+                args = args[i + 1:]
+                break
 
         option = disnake.utils.escape_mentions(random.choice(args))
         await inter.send(f"{option} {inter.author.mention}")
@@ -38,6 +44,32 @@ class Random(commands.Cog):
 
         option = str(random.randint(first, second))
         await inter.response.send_message(option)
+
+    @pick.error
+    @roll.error
+    @flip.error
+    async def command_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.message.channel.send(
+                utils.fill_message("bot_room_redirect", user=ctx.message.author.id, bot_room=config.bot_room)
+            )
+
+    def _channel_id(self, ctx):
+        return ctx.channel.parent_id if type(ctx.channel) == disnake.Thread else ctx.channel.id
+
+    async def cog_after_invoke(self, ctx):
+        channel_id = self._channel_id(ctx)
+        if channel_id not in config.allowed_channels:
+            await ctx.message.channel.send(
+                utils.fill_message("bot_room_redirect", user=ctx.message.author.id, bot_room=config.bot_room)
+            )
+
+    async def cog_check(self, ctx):
+        if not config.enable_room_check:
+            return True
+        if not ctx.guild:
+            return True
+        return self._channel_id(ctx) in config.allowed_channels
 
 
 def setup(bot):
