3191592fa6dc9941fbdf45fafdd3a111e1e81f80
Solumath <solumath99@gmail.com>
2022-12-11 20:17:15 +0100

    fix escape char, message for birthday api error

--- config/messages.py
+++ config/messages.py
@@ -267,6 +267,7 @@ class Messages:
     name_day_cz_brief = "Vypíše, kdo má dnes svátek"
     name_day_sk = "Dnes má meniny {name}"
     name_day_sk_brief = "Vypíše, kto má dnes meniny"
+    birthday_api_error = "Hobitovi to zas nefunguje, tak nevím kdo má dnes narozeniny <:sadge:751913081285902336>"
 
     repost_title = "Nápověda"
     repost_description = "{user}, shoda **{value}**!"
