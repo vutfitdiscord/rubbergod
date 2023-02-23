87742933310ff2f99908064fcfe973dcdd04a3b1
Solumath <solumath99@gmail.com>
2023-02-23 20:08:37 +0100

    change style of buttons and embed, wording

--- buttons/moderation.py
+++ buttons/moderation.py
@@ -3,15 +3,14 @@ from buttons.base import BaseView
 
 
 class ModerationView(BaseView):
-    def __init__(self, link, label, style, custom_id):
+    def __init__(self, link, label, custom_id):
         super().__init__(timeout=None)
         self.add_item(disnake.ui.Button(
             label=label,
-            style=style,
+            style=disnake.ButtonStyle.gray,
             custom_id=custom_id)
         )
         self.add_item(disnake.ui.Button(
             label="Original message",
-            style=disnake.ButtonStyle.primary,
             url=link)
         )
