01b5be6d38c32b765e3339713957f469e4c37a88
Solumath <solumath99@gmail.com>
2023-02-23 17:42:12 +0100

    forgot to use submod_helper_room, check on custom_id instead of label

--- buttons/moderation.py
+++ buttons/moderation.py
@@ -3,12 +3,12 @@ from buttons.base import BaseView
 
 
 class ModerationView(BaseView):
-    def __init__(self, link, label, style):
+    def __init__(self, link, label, style, custom_id):
         super().__init__(timeout=None)
         self.add_item(disnake.ui.Button(
             label=label,
             style=style,
-            custom_id="moderation:resolve")
+            custom_id=custom_id)
         )
         self.add_item(disnake.ui.Button(
             label="Original message",
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -14,9 +14,10 @@ import utils
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.moderation_true = "moderation:resolve:true"
+        self.moderation_false = "moderation:resolve:false"
 
-    async def mod_tag(self, message: disnake.Message, title):
-        mod_room = self.bot.get_channel(config.mod_room)
+    async def mod_tag(self, message: disnake.Message, title, room):
         if len(message.content) < 3800:
             embed = disnake.Embed(
                 title=f"Tag {title} used in #{message.channel.name}",
@@ -38,9 +39,14 @@ class Moderation(commands.Cog):
 
         embed.add_field(name="Resolved by:", value="---")
         embed.set_footer(text=datetime.now().strftime('%d.%m.%Y %H:%M'))
-        await mod_room.send(
+        await room.send(
             embed=embed,
-            view=ModerationView(message.jump_url, "Unresolved", disnake.ButtonStyle.gray)
+            view=ModerationView(
+                message.jump_url,
+                "Unresolved",
+                disnake.ButtonStyle.gray,
+                self.moderation_false
+            )
         )
 
     @commands.Cog.listener()
@@ -48,33 +54,36 @@ class Moderation(commands.Cog):
         """Logs use of @mod, @submod and @helper tag and send message to designated room"""
 
         if f"<@&{config.mod_role}>" in message.content:
-            await self.mod_tag(message, "@mod")
+            await self.mod_tag(message, "@mod", self.bot.get_channel(config.mod_room))
 
         elif f"<@&{config.submod_role}>" in message.content or f"<@&{config.helper_role}>" in message.content:
-            await self.mod_tag(message, "@submod/@helper")
+            await self.mod_tag(message, "@submod/@helper", self.bot.get_channel(config.submod_helper_room))
 
     @commands.Cog.listener()
     async def on_button_click(self, inter: disnake.MessageInteraction):
-        if inter.component.custom_id != "moderation:resolve":
+        if inter.component.custom_id not in [self.moderation_true, self.moderation_false]:
             return
 
         embed = inter.message.embeds[0].to_dict()
-        if inter.component.label == "Unresolved":
-            label = "Resolved"
-            style = disnake.ButtonStyle.green
-            for field in embed["fields"]:
-                if field["name"] == "Resolved by:":
-                    field["value"] = inter.author.mention
-        else:
+        if inter.component.custom_id == self.moderation_true:
             label = "Unresolved"
             style = disnake.ButtonStyle.gray
+            custom_id = self.moderation_false
             for field in embed["fields"]:
                 if field["name"] == "Resolved by:":
                     field["value"] = "---"
 
+        else:
+            label = "Resolved"
+            style = disnake.ButtonStyle.green
+            custom_id = self.moderation_true
+            for field in embed["fields"]:
+                if field["name"] == "Resolved by:":
+                    field["value"] = inter.author.mention
+
         await inter.response.edit_message(
             embed=disnake.Embed.from_dict(embed),
-            view=ModerationView(inter.message.jump_url, label, style)
+            view=ModerationView(inter.message.jump_url, label, style, custom_id)
         )
 
 
