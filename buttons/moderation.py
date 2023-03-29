b14ed130d67dbca8cced4edf69ce7f48ea5530a0
Solumath <solumath99@gmail.com>
2023-03-29 14:04:12 +0200

    remove url button for persistency of url

--- buttons/moderation.py
+++ buttons/moderation.py
@@ -4,14 +4,10 @@ from buttons.base import BaseView
 
 
 class ModerationView(BaseView):
-    def __init__(self, link, label, custom_id):
+    def __init__(self, label, custom_id):
         super().__init__(timeout=None)
         self.add_item(disnake.ui.Button(
             label=label,
             style=disnake.ButtonStyle.gray,
             custom_id=custom_id)
-        )
-        self.add_item(disnake.ui.Button(
-            label="Original message",
-            url=link)
         )
