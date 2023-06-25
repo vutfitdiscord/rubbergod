7fff9740085b86d79d2dd6528ab55db55adf109b
Solumath <solumath99@gmail.com>
2023-06-25 20:14:24 +0200

    bettermeme db rewrite

--- repository/database/better_meme.py
+++ repository/database/better_meme.py
@@ -1,11 +1,42 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from database import database, session
 from sqlalchemy import Column, Integer, String
 
-from repository.database import database
 
-
-class BetterMeme(database.base):
+class BetterMemeDB(database.base):
     __tablename__ = 'bot_better_meme'
 
     member_ID = Column(String, primary_key=True)
     posts = Column(Integer, default=0)
     total_karma = Column(Integer, default=0)
+
+    @classmethod
+    def get_member(cls, user_id: int) -> Optional[BetterMemeDB]:
+        return session.query(cls).filter(cls.member_ID == str(user_id)).one_or_none()
+
+    @classmethod
+    def add_post_to_repo(cls, user_id: int, karma: int) -> None:
+        member = cls.get_member(user_id)
+        if member is not None:
+            member.total_karma += karma
+            member.posts += 1
+            session.merge(member)
+            session.commit()
+        else:
+            session.add(cls(member_ID=str(user_id), posts=1, total_karma=karma))
+            session.commit()
+
+    @classmethod
+    def update_post_karma(cls, user_id: int, karma: int) -> None:
+        member = cls.get_member(user_id)
+        if member is not None:
+            member.total_karma += karma
+            session.merge(member)
+            session.commit()
+
+    @classmethod
+    def get_leaderboard(cls, order_by: str) -> List[BetterMemeDB]:
+        return session.query(cls).order_by(getattr(cls, order_by).desc())
