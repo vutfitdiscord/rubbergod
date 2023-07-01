355ccb1fe662ce2f1ea43b3f410a689f83e363d9
Peter Dragun <peter.dragun@espressif.com>
2023-07-01 15:48:52 +0200

    karma: rewrite DB to use classmethods

--- repository/base_repository.py
+++
@@ -1,8 +0,0 @@
-from config import messages
-from config.app_config import config
-
-
-class BaseRepository:
-    def __init__(self):
-        self.config = config
-        self.messages = messages.Messages
