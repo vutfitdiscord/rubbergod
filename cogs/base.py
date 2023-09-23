8a2ecccf3125176c6418c437eefa08d6a6dc8434
Solumath <solumath99@gmail.com>
2023-09-23 14:04:59 +0200

    blocked DMs & InteractionTimedOut
    refactor check if user blocked bot
    refactor check if InteractionTimedOut

--- cogs/base.py
+++ cogs/base.py
@@ -3,6 +3,8 @@ Base cog class. All cogs should inherit from this class.
 """
 from functools import cached_property
 
+import disnake
+
 from config.app_config import config
 
 
@@ -13,16 +15,24 @@ class Base:
         self.tasks = []
 
     @cached_property
-    def mod_room(self):
+    def mod_room(self) -> disnake.TextChannel:
         return self.bot.get_channel(self.config.mod_room)
 
     @cached_property
-    def submod_helper_room(self):
+    def submod_helper_room(self) -> disnake.TextChannel:
         return self.bot.get_channel(self.config.submod_helper_room)
 
     @cached_property
-    def log_channel(self):
+    def log_channel(self) -> disnake.TextChannel:
         return self.bot.get_channel(self.config.log_channel)
+
+    @cached_property
+    def bot_room(self) -> disnake.TextChannel:
+        return self.bot.get_channel(self.config.bot_room)
+
+    @cached_property
+    def bot_dev_channel(self) -> disnake.TextChannel:
+        return self.bot.get_channel(self.config.bot_dev_channel)
 
     def cog_unload(self) -> None:
         for task in self.tasks:
