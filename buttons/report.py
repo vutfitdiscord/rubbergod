dff74753a566549cd7fbb3c5f6661d11f22e374c
Solumath <solumath99@gmail.com>
2023-11-13 00:34:26 +0100

    report message and hints

--- buttons/report.py
+++ buttons/report.py
@@ -147,7 +147,7 @@ class ReportView(BaseView):
 
 
 class ReportGeneralView(ReportView):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
 
