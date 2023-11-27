e967b4a19b40c0c978038d564366fcf6386f55bb
Solumath <solumath99@gmail.com>
2023-11-27 21:58:45 +0100

    disable mention with sending birthday information

--- cogs/nameday.py
+++ cogs/nameday.py
@@ -84,7 +84,7 @@ class Nameday(Base, commands.Cog):
         name_day_sk = await self._name_day_sk()
         birthday = await self._birthday()
 
-        await self.bot_room.send(f"{name_day_cz}\n{name_day_sk}\n{birthday}")
+        await self.bot_room.send(f"{name_day_cz}\n{name_day_sk}\n{birthday}", allowed_mentions=False)
 
 
 def setup(bot):
