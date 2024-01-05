f60426d7c44df91421f6ad5406ccefc07a71a8c4
Lukas Tesar <lukastesar03@gmail.com>
2024-01-05 14:01:03 +0100

    Remove newline before defer

--- cogs/fun.py
+++ cogs/fun.py
@@ -68,7 +68,6 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="cat", description=Messages.fun_cat_brief)
     async def cat(self, inter):
         """Get random image of a cat"""
-
         await inter.response.defer()
 
         image_bytes, file_name = await self.get_image(inter, "https://api.thecatapi.com/v1/images/search")
@@ -98,7 +97,6 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="dog", description=Messages.fun_dog_brief)
     async def dog(self, inter):
         """Get random image of a dog"""
-
         await inter.response.defer()
 
         image_bytes, file_name = await self.get_image(inter, "https://api.thedogapi.com/v1/images/search")
@@ -128,7 +126,6 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="fox", description=Messages.fun_fox_brief)
     async def fox(self, inter):
         """Get random image of a fox"""
-
         await inter.response.defer()
 
         image_bytes, file_name = await self.get_image(inter, "https://randomfox.ca/floof/")
@@ -144,7 +141,6 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="duck", description=Messages.fun_duck_brief)
     async def duck(self, inter):
         """Get random image of a duck"""
-
         await inter.response.defer()
 
         image_bytes, file_name = await self.get_image(inter, "https://random-d.uk/api/v2/random")
@@ -164,7 +160,6 @@ class Fun(Base, commands.Cog):
         ---------
         keyword: search for a certain keyword in a joke
         """
-
         await inter.response.defer()
 
         if keyword is not None and ("&" in keyword or "?" in keyword):
@@ -213,7 +208,6 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="yo_mamajoke", description=Messages.fun_yo_mamajoke_brief)
     async def yo_mamajoke(self, inter):
         """Get random Yo momma joke"""
-
         await inter.response.defer()
 
         async with aiohttp.ClientSession() as session:
@@ -235,7 +229,6 @@ class Fun(Base, commands.Cog):
     @cooldowns.default_cooldown
     @commands.slash_command(name="fuchs", description=Messages.fun_fuchs_brief)
     async def fuchs(self, inter, hlaskaid: int = commands.Param(default=None, ge=1, le=len(fuchs_list))):
-
         await inter.response.defer()
 
         if len(fuchs_list) == 0:
