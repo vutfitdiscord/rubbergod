dd45391b9494129874f742d7089fb94fd45c3ec0
Lukáš Tesař <lukastesar03@gmail.com>
2024-01-05 14:12:18 +0100

    Add defers to commands in `fun` cog (#774)
    * Add defers to commands in `fun` cog

--- cogs/fun.py
+++ cogs/fun.py
@@ -68,6 +68,8 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="cat", description=Messages.fun_cat_brief)
     async def cat(self, inter):
         """Get random image of a cat"""
+        await inter.response.defer()
+
         image_bytes, file_name = await self.get_image(inter, "https://api.thecatapi.com/v1/images/search")
         image_file = disnake.File(image_bytes, filename=file_name)
 
@@ -95,6 +97,8 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="dog", description=Messages.fun_dog_brief)
     async def dog(self, inter):
         """Get random image of a dog"""
+        await inter.response.defer()
+
         image_bytes, file_name = await self.get_image(inter, "https://api.thedogapi.com/v1/images/search")
         image_file = disnake.File(image_bytes, filename=file_name)
 
@@ -122,6 +126,8 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="fox", description=Messages.fun_fox_brief)
     async def fox(self, inter):
         """Get random image of a fox"""
+        await inter.response.defer()
+
         image_bytes, file_name = await self.get_image(inter, "https://randomfox.ca/floof/")
         image_file = disnake.File(image_bytes, filename=file_name)
 
@@ -135,6 +141,8 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="duck", description=Messages.fun_duck_brief)
     async def duck(self, inter):
         """Get random image of a duck"""
+        await inter.response.defer()
+
         image_bytes, file_name = await self.get_image(inter, "https://random-d.uk/api/v2/random")
         image_file = disnake.File(image_bytes, filename=file_name)
 
@@ -152,6 +160,8 @@ class Fun(Base, commands.Cog):
         ---------
         keyword: search for a certain keyword in a joke
         """
+        await inter.response.defer()
+
         if keyword is not None and ("&" in keyword or "?" in keyword):
             await inter.send("I didn't find a joke like that.")
             return
@@ -198,6 +208,8 @@ class Fun(Base, commands.Cog):
     @commands.slash_command(name="yo_mamajoke", description=Messages.fun_yo_mamajoke_brief)
     async def yo_mamajoke(self, inter):
         """Get random Yo momma joke"""
+        await inter.response.defer()
+
         async with aiohttp.ClientSession() as session:
             async with session.get("https://api.yomomma.info/") as response:
                 if response.status != 200:
@@ -217,6 +229,8 @@ class Fun(Base, commands.Cog):
     @cooldowns.default_cooldown
     @commands.slash_command(name="fuchs", description=Messages.fun_fuchs_brief)
     async def fuchs(self, inter, hlaskaid: int = commands.Param(default=None, ge=1, le=len(fuchs_list))):
+        await inter.response.defer()
+
         if len(fuchs_list) == 0:
             inter.send(Messages.fun_fuchs_no_reaction)
             return
