60e879a4528af95236a6150b0aeb13a003891dca
Lukas Tesar <lukastesar03@gmail.com>
2024-01-05 13:58:12 +0100

    Add auto xlogin path generation

--- ssh_keygen.sh
+++ ssh_keygen.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
 # Generating keys for `/ios` and `/verify_db pull`
+printf "your xlogin00: "
+read xlogin
+# `ash` is not a mistake; Rubbergod Alpine does not have bash
 docker exec -i rubbergod-bot-1 ash << EOF
 set -e
 SSH_DIR="/root/.ssh" # Everything runs as root inside the container
@@ -6,7 +10,7 @@ mkdir -p "\$SSH_DIR"
 chmod 755 "\$SSH_DIR"
 echo "y" | ssh-keygen -q -N "" -t ed25519 -f "\$SSH_DIR"/leakcheck  1>/dev/null 2>&1
 echo "y" | ssh-keygen -q -N "" -t ed25519 -f "\$SSH_DIR"/etc_passwd 1>/dev/null 2>&1
-echo "command=\"/homes/eva/xl/xlogin00/rubbergod/leakcheck\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty" "\$(cat "\$SSH_DIR"/leakcheck.pub)"
+echo "command=\"/homes/eva/"${xlogin:0:2}"/${xlogin}/rubbergod/leakcheck\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty" "\$(cat "\$SSH_DIR"/leakcheck.pub)"
 echo "command=\"cat /etc/passwd\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty" "\$(cat "\$SSH_DIR"/leakcheck.pub)"
 EOF
 # Now, run this script and append its output to merlin:~/.ssh/authorized_keys.
