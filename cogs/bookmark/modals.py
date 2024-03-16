de8f59ab83126c541fea7e43fc145ace6a3f9eb8
Daniel Fajmon <dfajmon@redhat.com>
2024-03-16 19:41:35 +0100

    move bookmark to folder

--- cogs/bookmark/modals.py
+++ cogs/bookmark/modals.py
@@ -1,11 +1,11 @@
 import disnake
 
-from buttons.bookmark import BookmarkView
-from config.messages import Messages
-from features.bookmark import BookmarkFeatures
+from .features import Features
+from .messages_cz import MessagesCZ
+from .views import BookmarkView
 
 
-class BookmarkModal(disnake.ui.Modal):
+class Modal(disnake.ui.Modal):
     def __init__(self, message) -> None:
         self.message = message
         components = [
@@ -22,24 +22,26 @@ class BookmarkModal(disnake.ui.Modal):
 
     async def callback(self, inter: disnake.ModalInteraction) -> None:
         inter.message = self.message
-        title_name = Messages.bookmark_title(server=inter.guild.name)
+        title_name = MessagesCZ.bookmark_title(server=inter.guild.name)
 
         if not inter.text_values["name"] == "":
             title_name = inter.text_values["name"]
-        embed, images, files_attached = await BookmarkFeatures.create_bookmark_embed(self, inter, title_name)
+        embed, images, files_attached = await Features.create_bookmark_embed(self, inter, title_name)
 
         try:
             if images:
                 for image in images:
-                    embed.append(await BookmarkFeatures.create_image_embed(self, inter, image, title_name))
+                    embed.append(await Features.create_image_embed(self, inter, image, title_name))
             await inter.author.send(
                 embeds=embed, view=BookmarkView(inter.message.jump_url), files=files_attached
             )
             await inter.response.send_message(
-                Messages.bookmark_created(title_name=title_name), ephemeral=True
+                MessagesCZ.bookmark_created(title_name=title_name), ephemeral=True
             )
         except disnake.HTTPException as e:
             if e.code == 50007:
-                await inter.response.send_message(Messages.blocked_bot(user=inter.author.id), ephemeral=True)
+                await inter.response.send_message(
+                    MessagesCZ.blocked_bot(user=inter.author.id), ephemeral=True
+                )
             else:
                 raise e
