1988f51c67df079c3b4500ffe6460f8f160bb92e
Daniel Fajmon <dfajmon@redhat.com>
2024-03-16 19:41:35 +0100

    move bookmark to folder

--- buttons/general.py
+++ buttons/general.py
@@ -1,10 +1,11 @@
 import disnake
 
 from buttons.base import BaseView
+from config.messages import Messages
 
 
 class TrashView(BaseView):
-    def __init__(self, custom_id: str = "trash:delete", row: int = None):
+    def __init__(self, custom_id: str = Messages.trash_delete_id, row: int = None):
         super().__init__(timeout=None)
         self.add_item(
             disnake.ui.Button(emoji="🗑", style=disnake.ButtonStyle.danger, custom_id=custom_id, row=row),
