7298e37b7f4d2dd5eb4a3f00e31b12a3e37477bc
Solumath <solumath99@gmail.com>
2024-03-18 00:14:26 +0100

    fix naming

--- cogs/bookmark/cog.py
+++ cogs/bookmark/cog.py
@@ -8,8 +8,8 @@ from disnake.ext import commands
 from cogs.base import Base
 
 from .features import Features
-from .modals import BookmarkModal
-from .views import BookmarkView
+from .modals import Modal
+from .views import View
 
 
 class Bookmark(Base, commands.Cog):
@@ -20,7 +20,7 @@ class Bookmark(Base, commands.Cog):
     @commands.message_command(name="Bookmark", guild_ids=[Base.config.guild_id])
     async def bookmark(self, inter: disnake.ApplicationCommandInteraction, message: disnake.Message):
         """Send modal with input for bookmark name and then send to user"""
-        await inter.response.send_modal(modal=BookmarkModal(message))
+        await inter.response.send_modal(modal=Modal(message))
 
     async def bookmark_reaction(self, ctx):
         embed, images, files_attached = await Features.create_bookmark_embed(self, ctx)
@@ -29,9 +29,7 @@ class Bookmark(Base, commands.Cog):
                 embed.append(await Features.create_image_embed(self, ctx, image))
         # when sending sticker there can be overflow of files
         if len(files_attached) <= 10:
-            await ctx.member.send(embeds=embed, view=BookmarkView(ctx.message.jump_url), files=files_attached)
+            await ctx.member.send(embeds=embed, view=View(ctx.message.jump_url), files=files_attached)
         else:
-            await ctx.member.send(
-                embeds=embed, view=BookmarkView(ctx.message.jump_url), files=files_attached[:10]
-            )
+            await ctx.member.send(embeds=embed, view=View(ctx.message.jump_url), files=files_attached[:10])
             await ctx.member.send(files=files_attached[10:])
