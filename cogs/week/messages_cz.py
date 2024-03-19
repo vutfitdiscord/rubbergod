575cd3c41ca94c551f1f9fc8f01342a46c4b3cbb
Peter Dragun <peter.dragun@espressif.com>
2024-03-19 19:46:32 +0100

    refactor(week): move week cog to folder

---
+++ cogs/week/messages_cz.py
@@ -0,0 +1,6 @@
+from config.messages import Messages as GlobalMessages
+
+
+class MessagesCZ(GlobalMessages):
+    week_brief = "Vypíše, kolikátý je zrovna týden a jestli je sudý nebo lichý"
+    week_warning = "Pro sudý/lichý se využívá kalendářní týden"
