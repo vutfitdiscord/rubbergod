8ea5916045d200dd0efb68fa6eff1c00f8c00483
Solumath <solumath99@gmail.com>
2024-03-20 23:41:36 +0100

    migrate nameday to folder

--- cogs/nameday/cog.py
+++ cogs/nameday/cog.py
@@ -11,8 +11,9 @@ from disnake.ext import commands, tasks
 
 import utils
 from cogs.base import Base
-from config.messages import Messages
 from permissions import room_check
+
+from .messages_cz import MessagesCZ
 
 
 class Nameday(Base, commands.Cog):
@@ -31,7 +32,7 @@ class Nameday(Base, commands.Cog):
                 names = []
                 for i in res:
                     names.append(i["name"])
-                return Messages.name_day_cz(name=", ".join(names))
+                return MessagesCZ.name_day_cz(name=", ".join(names))
             except (asyncio.exceptions.TimeoutError, aiohttp.client_exceptions.ClientConnectorError):
                 return "Website unreachable"
 
@@ -44,7 +45,7 @@ class Nameday(Base, commands.Cog):
                 names = []
                 for i in res:
                     names.append(i["name"])
-                return Messages.name_day_sk(name=", ".join(names))
+                return MessagesCZ.name_day_sk(name=", ".join(names))
             except (asyncio.exceptions.TimeoutError, aiohttp.client_exceptions.ClientConnectorError):
                 return "Website unreachable"
 
@@ -57,15 +58,15 @@ class Nameday(Base, commands.Cog):
                     birthday = await resp.json()
                     return birthday["message"]
             except (asyncio.exceptions.TimeoutError, aiohttp.client_exceptions.ClientConnectorError):
-                return Messages.birthday_api_error
+                return MessagesCZ.birthday_api_error
 
-    @commands.slash_command(name="svatek", description=Messages.name_day_cz_brief)
+    @commands.slash_command(name="svatek", description=MessagesCZ.name_day_cz_brief)
     async def name_day_cz(self, inter: disnake.ApplicationCommandInteraction):
         await inter.response.defer(ephemeral=self.check.botroom_check(inter))
         name_day_cz = await self._name_day_cz()
         await inter.edit_original_response(name_day_cz)
 
-    @commands.slash_command(name="meniny", description=Messages.name_day_sk_brief)
+    @commands.slash_command(name="meniny", description=MessagesCZ.name_day_sk_brief)
     async def name_day_sk(self, inter: disnake.ApplicationCommandInteraction):
         await inter.response.defer(ephemeral=self.check.botroom_check(inter))
         name_day_sk = await self._name_day_sk()
@@ -78,7 +79,3 @@ class Nameday(Base, commands.Cog):
         birthday = await self._birthday()
         mentions = disnake.AllowedMentions.none()
         await self.bot_room.send(f"{name_day_cz}\n{name_day_sk}\n{birthday}", allowed_mentions=mentions)
-
-
-def setup(bot):
-    bot.add_cog(Nameday(bot))
