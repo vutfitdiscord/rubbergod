a516eddbd027539aa3ffae21f8806e7c70884c9e
Solumath <solumath99@gmail.com>
2024-03-20 23:31:32 +0100

    migrate fun to folder

---
+++ cogs/fun/messages_cz.py
@@ -0,0 +1,12 @@
+from config.messages import Messages as GlobalMessages
+
+
+class MessagesCZ(GlobalMessages):
+    cat_brief = "Pošle náhodný obrázek kočky"
+    dog_brief = "Pošle náhodný obrázek psa"
+    fox_brief = "Pošle náhodný obrázek lišky"
+    duck_brief = "Pošle náhodný obrázek kachny"
+    dadjoke_brief = "Pošle náhodný dadjoke nebo vyhledá podle zadaného slova"
+    yo_mamajoke_brief = "Pošle náhodný Yo momma joke"
+    fuchs_brief = "Pošle náhodnou fuchs reakci"
+    fuchs_no_reaction = "Žádná fuchs reakce není k dispozici"
