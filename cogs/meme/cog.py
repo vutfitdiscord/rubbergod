902d2c3f7f32e0eabfaf7494cff907a990aa477c
Solumath <solumath99@gmail.com>
2024-03-21 15:15:58 +0100

    migrate meme to folder

--- cogs/meme/cog.py
+++ cogs/meme/cog.py
@@ -6,7 +6,8 @@ import disnake
 from disnake.ext import commands
 
 from cogs.base import Base
-from config.messages import Messages
+
+from .messages_cz import MessagesCZ
 
 uhoh_counter = 0
 
@@ -15,6 +16,10 @@ class Meme(Base, commands.Cog):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
+
+    @commands.slash_command(name="uhoh", description=MessagesCZ.uhoh_brief)
+    async def uhoh(self, inter: disnake.ApplicationCommandInteraction):
+        await inter.send(MessagesCZ.uhoh_counter(uhohs=uhoh_counter))
 
     @commands.Cog.listener()
     async def on_message(self, message: disnake.Message):
@@ -33,20 +38,12 @@ class Meme(Base, commands.Cog):
             await message.channel.send("uh oh")
             uhoh_counter += 1
         elif message.content == "PR":
-            await message.channel.send(Messages.pr_meme)
+            await message.channel.send(MessagesCZ.pr_meme)
 
     @commands.Cog.listener()
     async def on_raw_message_delete(self, payload: disnake.RawMessageDeleteEvent):
         if payload.channel_id == self.config.upgraded_pocitani_thread_id:
             pocitani = self.bot.get_channel(payload.channel_id)
             startnum = self.config.upgraded_pocitani_start_num
-            await pocitani.send(Messages.upgraded_pocitani_caught_deleting)
+            await pocitani.send(MessagesCZ.upgraded_pocitani_caught_deleting)
             await pocitani.send(startnum)
-
-    @commands.slash_command(name="uhoh", description=Messages.uhoh_brief)
-    async def uhoh(self, inter):
-        await inter.send(Messages.uhoh_counter(uhohs=uhoh_counter))
-
-
-def setup(bot):
-    bot.add_cog(Meme(bot))
---
+++ cogs/meme/messages_cz.py
@@ -0,0 +1,8 @@
+from config.messages import Messages as GlobalMessages
+
+
+class MessagesCZ(GlobalMessages):
+    uhoh_counter = "{uhohs} uh ohs od spuštění."
+    uhoh_brief = "Vypíše počet uh ohs od spuštění"
+    uhoh = "uh oh"
+    pr_meme = "https://github.com/Toaster192/rubbergod/pulls"
