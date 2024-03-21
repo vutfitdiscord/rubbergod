d49911391d9a109e879503d3d0bff58ad5e68738
Solumath <solumath99@gmail.com>
2024-03-21 15:13:40 +0100

    migrate latex to folder

--- config/messages.py
+++ config/messages.py
@@ -446,18 +446,6 @@ class Messages(metaclass=Formatable):
     config_list_invalid_regex = "Chybný regex\n`{regex_err}`"
     config_synced = "Config successfully synchronized."
 
-    # LATEX
-    latex_help = f"Příklad:\n`{prefix}latex [?fg=blue] x^n + y^n = z^n`"
-    latex_desc = "Vykreslí LaTeX výraz"
-    latex_colors = """**Možné barvy textu:**
-    Transparent White Black Blue Cyan Green Magenta Red Yellow Orange Maroon NavyBlue RoyalBlue
-    ProcessBlue SkyBlue Turquoise TealBlue Aquamarine BlueGreen Sepia Brown Tan Gray Fuchsia
-    Lavender Purple Plum Violet GreenYellow Goldenrod Dandelion Apricot Peach Melon YellowOrange
-    BurntOrange Bittersweet RedOrange Mahogany BrickRed OrangeRed RubineRed WildStrawberry Salmon
-    CarnationPink VioletRed Rhodamine Mulberry RedViolet Thistle Orchid DarkOrchid RoyalPurple BlueViolet
-    Periwinkle CadetBlue CornflowerBlue MidnightBlue Cerulean Emerald JungleGreen SeaGreen ForestGreen
-    PineGreen LimeGreen YellowGreen SpringGreen OliveGreen RawSienna"""
-
     # MEME REPOST
     meme_repost_link = "[Odkaz na originál]({original_message_url}) v <#{original_channel}>"
     meme_leaderboard_brief = "#better-memes leaderboard"
