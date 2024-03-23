db233da2012b768f4f3614746e737f67f2c9fd42
Solumath <solumath99@gmail.com>
2024-03-23 12:22:55 +0100

    migrate moderation to folder

---
+++ cogs/dynamicconfig/messages_cz.py
@@ -0,0 +1,17 @@
+from config.messages import Messages as GlobalMessages
+
+
+class MessagesCZ(GlobalMessages):
+    set_brief = "Nastaví hodnotu v konfiguraci"
+    append_brief = "Přidá hodnotu do pole v konfiguraci"
+    load_brief = "Znovu načte třídu ze souboru. Pro aplikování změn je potřeba znovu načíst i cog"
+    list_brief = "Vypíše klíče konfigurace"
+    get_brief = "Získá hodnotu z konfigurace"
+    backup_brief = "Vytvoří záložní kopii konfigurace v novém souboru. Záloha bude obsahovat dnešní datum"
+    sync_template_brief = "Přidá nové klíče z template do konfigurace"
+    wrong_key = "Nesprávný klíč"
+    wrong_type = "Nesprávný typ"
+    backup_created = "Záloha konfigurace vytvořena."
+    list_invalid_regex = "Chybný regex\n`{regex_err}`"
+    config_updated = "Konfigurace aktualizována."
+    config_loaded = "Konfigurace načtena."
