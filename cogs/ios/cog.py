4bed8f10155a94716eb81540390f9fa43464c2e3
Solumath <solumath99@gmail.com>
2024-03-24 00:49:38 +0100

    migrate ios to folder

--- cogs/ios/cog.py
+++ cogs/ios/cog.py
@@ -12,11 +12,12 @@ from disnake.ext import commands, tasks
 import utils
 from cogs.base import Base
 from config import cooldowns
-from config.messages import Messages
 from database import session
 from database.verification import PermitDB, ValidPersonDB
 from features.list_message_sender import send_list_of_messages
 from permissions import permission_check
+
+from .messages_cz import MessagesCZ
 
 
 def running_for(time):
@@ -236,14 +237,14 @@ async def print_output(bot, channel, system, resources):
 
 
 class IOS(Base, commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot
         self.tasks = [self.ios_task]
 
     @cooldowns.default_cooldown
     @commands.check(permission_check.helper_plus)
-    @commands.slash_command(name="ios", description=Messages.ios_brief, guild_ids=[Base.config.guild_id])
+    @commands.slash_command(name="ios", description=MessagesCZ.ios_brief, guild_ids=[Base.config.guild_id])
     async def ios(self, inter: disnake.ApplicationCommandInteraction):
         await inter.response.defer()
         await self.ios_task(inter)
@@ -253,40 +254,40 @@ class IOS(Base, commands.Cog):
         pass
 
     @commands.check(permission_check.is_bot_admin)
-    @_ios.sub_command(name="start", description=Messages.ios_task_start_brief)
+    @_ios.sub_command(name="start", description=MessagesCZ.task_start_brief)
     async def ios_task_start(self, inter: disnake.ApplicationCommandInteraction):
         try:
             self.ios_task.start()
-            await inter.send(Messages.ios_task_start_success)
+            await inter.send(MessagesCZ.task_start_success)
         except RuntimeError:
-            await inter.send(Messages.ios_task_start_already_set)
+            await inter.send(MessagesCZ.task_start_already_set)
 
     @commands.check(permission_check.is_bot_admin)
-    @_ios.sub_command(name="stop", description=Messages.ios_task_stop_brief)
+    @_ios.sub_command(name="stop", description=MessagesCZ.task_stop_brief)
     async def ios_task_stop(self, inter: disnake.ApplicationCommandInteraction):
         if self.ios_task.is_running():
             self.ios_task.stop()
-            await inter.send(Messages.ios_task_stop_success)
+            await inter.send(MessagesCZ.task_stop_success)
         else:
-            await inter.send(Messages.ios_task_stop_nothing_to_stop)
+            await inter.send(MessagesCZ.task_nothing_to_stop)
 
     @commands.check(permission_check.is_bot_admin)
-    @_ios.sub_command(name="cancel", description=Messages.ios_task_cancel_brief)
+    @_ios.sub_command(name="cancel", description=MessagesCZ.task_cancel_brief)
     async def ios_task_cancel(self, inter: disnake.ApplicationCommandInteraction):
         if self.ios_task.is_running():
             self.ios_task.cancel()
-            await inter.send(Messages.ios_task_stop_success)
+            await inter.send(MessagesCZ.task_stop_success)
         else:
-            await inter.send(Messages.ios_task_stop_nothing_to_stop)
+            await inter.send(MessagesCZ.task_nothing_to_stop)
 
     @tasks.loop(minutes=Base.config.ios_looptime_minutes)
     async def ios_task(self, inter: disnake.ApplicationCommandInteraction = None):
         # Respond to interaction if any, else print everything to #ios-private
         channel = inter.channel if inter is not None else self.bot.get_channel(self.config.ios_channel_id)
         if inter is not None:
-            await inter.edit_original_response(Messages.ios_howto_clean)
+            await inter.edit_original_response(MessagesCZ.howto_clean)
         else:
-            await channel.send(Messages.ios_howto_clean)
+            await channel.send(MessagesCZ.howto_clean)
 
         process = subprocess.Popen(
             ["ssh", "-i", self.config.ios_leakcheck_key_path, "merlin"], stdout=subprocess.PIPE
@@ -306,7 +307,7 @@ class IOS(Base, commands.Cog):
             }
             await print_output(self.bot, channel, "merlinovi", filter_year(parsed_resources))
         except (IndexError, ValueError) as e:
-            await channel.send(Messages.ios_parsing_error)
+            await channel.send(MessagesCZ.parsing_error)
             # Send it to bot-dev channel anyway
             raise e
 
@@ -330,11 +331,7 @@ class IOS(Base, commands.Cog):
             }
             await print_output(self.bot, channel, "evě", filter_year(parsed_resources))
         except (IndexError, ValueError) as e:
-            await channel.send(Messages.ios_parsing_error)
+            await channel.send(MessagesCZ.parsing_error)
             # Send it to bot-dev channel anyway
             raise e
         # eva doesn't seem to have /dev/shm
-
-
-def setup(bot):
-    bot.add_cog(IOS(bot))
---
+++ cogs/ios/messages_cz.py
@@ -0,0 +1,15 @@
+from config.messages import Messages as GlobalMessages
+from config.app_config import config
+
+
+class MessagesCZ(GlobalMessages):
+    ios_brief = "Připomene všem prasatům, že si mají jít po sobě uklidit"
+    task_start_brief = "Začne pravidelně připomínat všem prasatům, že si mají jít po sobě uklidit"
+    task_start_success = f"Automatické připomínání úspěšně nastaveno. Bude se od teď provádět každých {config.ios_looptime_minutes} minut."
+    task_start_already_set = "Automatické připomínání už je nastaveno."
+    task_stop_brief = "Zastaví automatické připomínání"
+    task_stop_success = "Automatické připomínání zastaveno."
+    task_nothing_to_stop = "Automatické připomínání není nastaveno."
+    task_cancel_brief = "Okamžitě ukončí automatické připomínání (přeruší aktuální běh)"
+    parsing_error = "Toastere, máš bordel v parsování."
+    howto_clean = "Pokud nevíte, jak po sobě uklidit, checkněte: https://discordapp.com/channels/461541385204400138/534431057001316362/698701631495340033"
