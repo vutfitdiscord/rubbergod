9eec915aa99dd910416d167d016dd2bc352c92a0
Solumath <solumath99@gmail.com>
2024-03-23 11:29:43 +0100

    migrate icons to folder

---
+++ cogs/icons/messages_cz.py
@@ -0,0 +1,10 @@
+from config.messages import Messages as GlobalMessages
+
+
+class MessagesCZ(GlobalMessages):
+    icon_brief = "UI pro přiřazení ikony"
+    set_success = "Změna ikony na {icon} proběhla úspěšně"
+    choose_icon = "Vyber si ikonu"
+    fail_icon = "Nastavit ikonu se nepodařilo"
+    no_permission = "Na tuto ikonu nemáš právo"
+    icon_removed = "Ikona byla odstraněna"
