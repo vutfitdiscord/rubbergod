ddf2f1be0e11dcdf01c9185f7a9f9db0badf21c3
Solumath <solumath99@gmail.com>
2024-03-23 15:39:11 +0100

    refactor moderation cog

--- cogs/moderation/features.py
+++ cogs/moderation/features.py
@@ -8,11 +8,14 @@ from .views import View
 
 SLOWMODE_CHANNEL_TYPES = disnake.TextChannel | disnake.Thread | disnake.VoiceChannel | disnake.ForumChannel
 
+MODERATION_TRUE = "moderation:resolve:true"
+MODERATION_FALSE = "moderation:resolve:false"
 
-async def mod_tag(self, message: disnake.Message, title, room):
+
+async def mod_tag(message: disnake.Message, role: str, room: disnake.TextChannel):
     if len(message.content) < 3800:
         embed = disnake.Embed(
-            title=f"Tagged {title}",
+            title=f"Tagged {role}",
             description=f"**User:** {message.author.mention}\n"
             f"**Link:** [#{message.channel}]({message.jump_url})\n"
             f"**Content:**\n{message.content}",
@@ -20,7 +23,7 @@ async def mod_tag(self, message: disnake.Message, title, room):
         )
     else:
         embed = disnake.Embed(
-            title=f"Tagged {title}",
+            title=f"Tagged {role}",
             description=f"**User:** {message.author.mention}\n"
             f"**Link:** [#{message.channel}]({message.jump_url})\n",
             color=disnake.Color.yellow(),
@@ -31,24 +34,23 @@ async def mod_tag(self, message: disnake.Message, title, room):
 
     embed.add_field(name="Resolved by:", value="---")
     embed.set_footer(text=datetime.now().strftime("%d.%m.%Y %H:%M"))
-    await room.send(embed=embed, view=View("Resolve", self.moderation_false))
+    await room.send(embed=embed, view=View("Resolve", MODERATION_FALSE))
 
 
 async def log(
-    self,
     inter: disnake.GuildCommandInteraction,
     prev_delay: int,
     curr_delay: int,
     channel: SLOWMODE_CHANNEL_TYPES,
+    log_channel: disnake.TextChannel,
 ):
     """
     Log slowmode changes
     """
     embed = disnake.Embed(title="Channel slowmode change", color=disnake.Colour.yellow())
-    embed.add_field(name="Mod", value=f"{inter.author.mention} ({inter.author.display_name})")
+    embed.add_field(name="Mod", value=f"{inter.author.mention} ({inter.author.name})")
     embed.add_field(name="Channel", value=f"[#{channel.name}]({channel.jump_url})", inline=False)
     embed.add_field(name="Old value", value=f"{prev_delay} seconds")
     embed.add_field(name="New value", value=f"{curr_delay} seconds")
     embed.timestamp = datetime.now(tz=timezone.utc)
-    channel = self.bot.get_channel(self.config.log_channel)
-    await channel.send(embed=embed)
+    await log_channel.send(embed=embed)
