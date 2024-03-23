977b88d568754446f6ac52753ce95424ac0460e0
Peter Dragun <peter.dragun@espressif.com>
2024-03-23 13:32:03 +0100

    refactor(review): move review to a separate folder

--- config/messages.py
+++ config/messages.py
@@ -266,58 +266,6 @@ class Messages(metaclass=Formatable):
     vote_result_multiple = "V hlasování „{question}“ vyhrály možnosti {winning_emojis} s {votes} hlasy."
     vote_result_none = "V hlasování „{question}“ nikdo nehlasoval. <:sadcat:576171980118687754>"
 
-    # REVIEW
-    review_add_brief = "Přidá recenzi na předmět. Pokud jsi již recenzi na předmět napsal, bude nahrazena novou."
-    review_get_brief = "Vypíše recenze na vybraný předmět"
-    review_remove_brief = "Odstraní hodnocení"
-    review_list_brief = "Vypíše předměty, které si již ohodnotil"
-    review_id_brief = "ID recenze, pouze pro administrátory"
-    review_grade_brief = "Známku, kterou by jsi dal předmětu od A-F (za organizaci, splnění očekávání, kvalitu výuky ...)"
-
-    review_wrong_subject = "Nesprávná zkratka předmětu."
-    review_added = "Hodnocení předmětu bylo přidáno."
-    reviews_reaction_help = "Pokud byla recenze užitečná dejte 👍, jinak 👎.\n" \
-                            "Pro odstranění hlasu je možné použit 🛑.\n" \
-                            "Použijte reakce ◀️ a ▶️ pro navigaci mezi recenzemi.\n" \
-                            "Pro navigaci v textu delších recenzí použijte 🔼 a 🔽.\n"
-    review_vote_own = "Nemůžeš hlasovat pro vlastní recenzi"
-    review_remove_success = "Hodnocení předmětu bylo odebráno."
-    review_not_found = "Hodnocení předmětu nebylo nalezeno."
-    review_remove_denied = "{user}, `id` je pouze pro administrátory. Pro smazání použij jen `subject`."
-    review_add_denied = "{user}, na přidání hodnocení předmětu nemáš právo."
-    review_not_on_server = "{user}, na použití tohoto příkazu musíš být na FITwide serveru."
-
-    # review embed
-    review_embed_description = "{name}\n**Průměrná známka od studenta:** {grade}"
-    review_embed_no_reviews = "*Zatím nic*"
-    review_text_label = "Text recenze"
-    review_text_page_label = "Stránka textu"
-    review_author_label = "Autor"
-    review_grade_label = "Kvalita předmětu"
-    review_date_label = "Datum"
-    review_other_reviews_label = "Další hodnocení"
-    review_authored_list_label = "Ohodnotil jsi:"
-
-    # review modal
-    review_modal_title = "Přidat novou recenzi"
-    review_subject_label = "Vyberte předmět"
-    review_anonym_label = "Anonymní recenze"
-    review_signed_label = "Zobrazit nick"
-    review_tier_placeholder = "Hodnocení předmětu"
-    review_tier_0_desc = "Nejlepší, jednoduchý, naučí"
-    review_tier_1_desc = "Naučí, ale je třeba zapracovat"
-    review_tier_2_desc = "Průměrný předmět"
-    review_tier_3_desc = "Nic moc"
-    review_tier_4_desc = "Nejhorší, celé zle"
-
-    subject_update_biref = "Automaticky vyhledá a přidá předměty do reviews i subject databáze"
-    subject_update_overwrite_brief = "Přepíše všechny informace o předmětech. Využít pouze v kombinaci s další aktualizací bez přepisu."
-    subject_update_error = "Aktualizace se nezdařila pro <{url}>\n"
-    subject_update_success = "Předměty byly aktualizovány."
-    shortcut_brief = "Vrací stručné informace o předmětu"
-    tierboard_brief = "Založeno na `reviews` z průměru tier hodnot"
-    tierboard_missing_year = f"Nemáš ročníkovou roli, prosím specifikuj ročník"
-
     # WARDEN
     warden_scan_brief = "Prohledá obrázky v aktuálním kanále a uloží je jako hash pro detekci repostu.\nlimit: [all | <int>]"
     repost_title = "Nápověda"
