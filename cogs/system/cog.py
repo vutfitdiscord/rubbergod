5bf272220c1285c7481bb247a95a6a4a31cf3ac6
Solumath <solumath99@gmail.com>
2024-03-26 13:34:38 +0100

    refactor system to accept cogs as modules

--- cogs/system/cog.py
+++ cogs/system/cog.py
@@ -2,13 +2,11 @@
 Core cog for bot. Can't be unloaded. Contains commands for cog management.
 """
 
-import math
 import subprocess
 from datetime import datetime
 
 import disnake
 from disnake.ext import commands
-from disnake.message import Message
 
 import utils
 from cogs.base import Base
@@ -20,7 +18,7 @@ from permissions import permission_check
 
 from . import features
 from .messages_cz import MessagesCZ
-from .views import Dropdown, SystemView
+from .views import View
 
 boottime = datetime.now().replace(microsecond=0)
 
@@ -36,13 +34,13 @@ class System(Base, commands.Cog):
 
     @commands.check(permission_check.is_bot_admin)
     @commands.slash_command(name="git")
-    async def git(self, inter):
+    async def git(self, inter: disnake.ApplicationCommandInteraction):
         pass
 
     @git.sub_command(name="pull", description=MessagesCZ.git_pull_brief)
     async def pull(self, inter: disnake.ApplicationCommandInteraction):
         await inter.send("Pulling...")
-        message: Message = await inter.original_message()
+        message: disnake.Message = await inter.original_message()
 
         pull_result = await self.git.pull()
         pull_parts = utils.cut_string(pull_result, 1900)
@@ -86,21 +84,6 @@ class System(Base, commands.Cog):
         await inter.send("Shutting down...")
         exit(1)
 
-    async def create_selects(self):
-        """Slices dictionary of all cogs to chunks for select."""
-        cog_files = list(features.get_all_cogs().keys())
-        cog_names = list(features.get_all_cogs().values())
-        all_selects = []
-
-        # 25 is max number of options for one select
-        chunks = math.ceil(len(cog_files) / 25)
-        cog_files = list(utils.split(cog_files, chunks))
-        cog_names = list(utils.split(cog_names, chunks))
-        for i in range(0, chunks):
-            all_selects.append([cog_files[i], cog_names[i]])
-
-        return all_selects
-
     @commands.check(permission_check.is_bot_admin)
     @commands.slash_command(name="cogs", description=MessagesCZ.cogs_brief, guild_ids=[Base.config.guild_id])
     async def cogs(self, inter: disnake.ApplicationCommandInteraction):
@@ -109,17 +92,16 @@ class System(Base, commands.Cog):
 
         Max number of cogs can be 100 (4x25).
         """
-
-        selects = await self.create_selects()
-        view = SystemView(self.bot, len(selects), selects)
-        embed = Dropdown.create_embed(self, inter.author.color)
-        await inter.send(embed=embed, view=view)
+        await inter.response.defer()
+        cogs = await features.split_cogs()
+        view = View(self.bot, cogs)
+        embed = features.create_embed(self.bot)
+        message = await inter.followup.send(embed=embed, view=view)
 
         # pass message object to classes
-        message = await inter.original_message()
         view.message = message
-        for i, cogs in enumerate(selects):
-            view.selects[i].msg = message
+        for i in range(len(cogs)):
+            view.selects[i].message = message
 
     @cooldowns.default_cooldown
     @commands.slash_command(name="uptime", description=MessagesCZ.uptime_brief)
@@ -144,14 +126,3 @@ class System(Base, commands.Cog):
         )
         self.error_log.set_image(embed, self.bot.user, count)
         await inter.edit_original_response(embed=embed)
-
-    @pull.error
-    @cogs.error
-    async def on_command_error(self, ctx: commands.Context, error):
-        if isinstance(error, commands.errors.CommandInvokeError):
-            if isinstance(error.__cause__, commands.errors.ExtensionAlreadyLoaded):
-                await ctx.send(MessagesCZ.cog_is_loaded(cog=error.__cause__.name))
-                return True
-            elif isinstance(error.__cause__, commands.errors.ExtensionNotLoaded):
-                await ctx.send(MessagesCZ.cog_is_unloaded(cog=error.__cause__.name))
-                return True
