21e67a0e4b840fef988f12d6c32fcb03cc284c77
Solumath <solumath99@gmail.com>
2024-03-29 16:18:35 +0100

    migrate memerepost and rename to bettermeme

---
+++ cogs/bettermeme/messages_cz.py
@@ -0,0 +1,7 @@
+from config.messages import Messages as GlobalMessages
+
+
+class MessagesCZ(GlobalMessages):
+    better_meme_link = "[Odkaz na originál]({original_message_url}) v <#{original_channel}>"
+    leaderboard_brief = "#better-memes leaderboard"
+    board_start_param = "Zobrazí leaderboard na dané pozici"
