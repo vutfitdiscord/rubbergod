f9da494504212154097f118c9a2fdd9b205e17ee
Solumath <solumath99@gmail.com>
2024-03-30 21:09:10 +0100

    migrate vote to folder

--- config/messages.py
+++ config/messages.py
@@ -141,30 +141,6 @@ class Messages(metaclass=Formatable):
     dynamic_verify_no_roles = "Nebyla nalezena žádná role."
     dynamic_verify_missing_rule = "Toto pravidlo (`{rule_id}`) neexistuje."
 
-    # VOTE
-    vote_brief = "Zahájí hlasování, ve kterém je možné zvolit více možností"
-    vote_one_of_brief = "Zahájí hlasování, ve kterém je možné zvolit pouze jednu možnost"
-    vote_format = f"`{prefix}[single]vote [datum a čas konce]\n[otázka]\n[emoji] " \
-                  "[možnost 1]\n[emoji] [možnost 2]\na tak dále`\n" \
-                  "Jako datum/čas to sežere skoro všechno, před otázkou newline pls.\n" \
-                  "Datum a čas jsou nepovinné argumenty. " \
-                  "Pokud jsou vyplněny, bot pošle po uplynutí zprávu o výsledku. " \
-                  "Příkaz singlevote vytvoří hlasování, kde lze zvolit jen jednu možnost."
-
-    vote_not_emoji = "Na začátku možnosti '{opt}' není emoji. <:sadcat:576171980118687754>"
-    vote_bad_date = "Hlasování může skončit jen v budoucnosti. <:objection:490989324125470720>"
-    vote_bad_format = "Špatný formát hlasování. <:sadcat:576171980118687754>"
-
-    vote_winning = "Prozatím vyhrává možnost {winning_emoji} „{winning_option}“ s {votes} hlasy."
-    vote_winning_multiple = "Prozatím vyhrávají možnosti {winning_emojis} s {votes} hlasy."
-
-    vote_none = "Čekám na hlasy."
-
-    vote_result = "V hlasování „{question}“ vyhrála možnost {winning_emoji} " \
-                  "„{winning_option}“ s {votes} hlasy."
-    vote_result_multiple = "V hlasování „{question}“ vyhrály možnosti {winning_emojis} s {votes} hlasy."
-    vote_result_none = "V hlasování „{question}“ nikdo nehlasoval. <:sadcat:576171980118687754>"
-
     # WARDEN
     warden_scan_brief = "Prohledá obrázky v aktuálním kanále a uloží je jako hash pro detekci repostu.\nlimit: [all | <int>]"
     repost_title = "Nápověda"
