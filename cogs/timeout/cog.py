a2afa61a6eca5b22672c1ffaa910d5ffcb260659
Solumath <solumath99@gmail.com>
2024-03-31 18:54:08 +0200

    timeoutwars update

--- cogs/timeout/cog.py
+++ cogs/timeout/cog.py
@@ -363,7 +363,11 @@ class Timeout(Base, commands.Cog):
                 # add timeout manually
                 length = entry.changes.after.timeout - entry.created_at
                 length = timedelta(seconds=math.ceil(length.total_seconds()))  # round up to seconds
-                reason = entry.reason or MessagesCZ.manual_timeout
+                reason = entry.reason or MessagesCZ.timeout_manual_timeout
+                if reason == MessagesCZ.timeout_wars_reason:
+                    # timeout was added during timeout wars 1. April
+                    return
+
                 TimeoutDB.add_timeout(
                     entry.target.id,
                     entry.user.id,
