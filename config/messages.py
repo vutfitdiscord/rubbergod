dad54f331c9867a3be0fc3b0e7e106bb1cdb165d
Solumath <solumath99@gmail.com>
2024-03-31 18:54:08 +0200

    timeoutwars update

--- config/messages.py
+++ config/messages.py
@@ -49,3 +49,4 @@ class Messages(metaclass=Formatable):
     timeout_wars_user = "Uživatel {user} byl umlčen na {time:.0f} minut."
     timeout_wars_message_delete = "Uživatel {user} byl přistižen při mazání zpráv. Byl proto umlčen na {time:.0f} minut."
     timeout_wars_user_immunity = "Uživatel {user} má ještě imunitu na {time:.0f} sekund."
+    timeout_wars_reason = "timeoutwars"
