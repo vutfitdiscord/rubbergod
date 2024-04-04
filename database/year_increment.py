94c61b336d2855a7b5bc7a18111f25a95111ef95
Solumath <solumath99@gmail.com>
2024-04-04 21:33:40 +0200

    mypy fix: roles

--- database/year_increment.py
+++ database/year_increment.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String
 from database import database
 
 
-class User_backup(database.base):
+class User_backup(database.base):  # type: ignore
     __tablename__ = "bot_user_backup"
 
     member_ID = Column(String, primary_key=True)
