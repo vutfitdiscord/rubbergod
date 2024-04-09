b91695a4e150458b8e47d60ab20a3669990bea77
Solumath <solumath99@gmail.com>
2024-04-09 16:57:54 +0200

    use messages in info

--- cogs/info/messages_cz.py
+++ cogs/info/messages_cz.py
@@ -5,7 +5,6 @@ class MessagesCZ(GlobalMessages):
     urban_brief = "Vyhledávaní výrazu v urban slovníku"
     urban_not_found = "Pro daný výraz neexistuje záznam <:sadcat:576171980118687754>"
 
-    weather_brief = "Vypíše informace o počasí ve zvoleném městě. Výchozí město je Brno."
     credit_limit_brief = "Vypíše, jak to funguje s ročním kreditovým stropem."
     credit_limit_info = """```cs
 if ("pokazil jsem volitelný" or "Pokazil jsem aspoň 2 povinné")     \n  return 65
@@ -13,3 +12,18 @@ if ("Pokazil jsem 1 povinný" or "Mám průměr 2.0 nebo více než 2.0") \n  re
 if ("Mám průměr pod 1.5")                                           \n  return 80
 if ("Mám průměr pod 2.0")                                           \n  return 75
 ```"""
+
+    # Weather / Počasí
+    weather_brief = "Vypíše informace o počasí ve zvoleném městě. Výchozí město je Brno."
+    weather_description = "Aktuální počasí v městě {city}, {country}"
+    invalid_name = "Takhle se žádné město určitě nejmenuje."
+    city_not_found = "Město nenalezeno"
+    token_error = "Rip token -> Rebel pls fix"
+    no_city = "Město nenalezeno! <:pepeGun:484470874246742018> ({result})"
+    weather = "Počasí"
+    temperature = "Teplota"
+    feels_like = "Pocitová teplota"
+    humidity = "Vlhkost"
+    wind = "Vítr"
+    clouds = "Oblačnost"
+    visibility = "Viditelnost"
