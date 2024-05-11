491dee3c7a31cf8248e036fafcc6f5306bb0550a
Solumath <solumath99@gmail.com>
2024-05-11 20:15:21 +0200

    remove blocking requests in gif.py

--- cogs/gif/messages_cz.py
+++ cogs/gif/messages_cz.py
@@ -5,4 +5,3 @@ class MessagesCZ(GlobalMessages):
     bonk_brief = "Bonk na uživatele"
     unsupported_image = "Tento avatar aktuálne není podporovaný <:sadcat:576171980118687754>"
     pet_brief = "Vytvoří gif z uživatele."
-    gif_req_error = "Nepodařilo se získat profilový obrázek uživatele."
