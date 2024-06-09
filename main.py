047c339485ecb6dc9b7aa137b01f7e9c24104008
Solumath <solumath99@gmail.com>
2024-06-09 15:31:38 +0200

    move presence

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import database.db_migrations as migrations
 from config.app_config import config
 from features.error import ErrorLogger
 from features.logger import setup_logging
-from features.presence import Presence
 from rubbergod import Rubbergod
 
 setup_logging()
@@ -22,7 +21,6 @@ class Bot(Rubbergod):
     def __init__(self):
         super().__init__()
         # Features
-        self.presence = Presence(self)
         self.err_logger = ErrorLogger(self)
 
         self.init_cogs()
