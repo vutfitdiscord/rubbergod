1075ed19408b94abc57f58a1e8f52f5f52c41368
Marián Marek <contact@marianmarek.sk>
2024-08-02 11:31:01 +0200

    fix lottie sticker bookmark

--- cogs/bookmark/messages_cz.py
+++ cogs/bookmark/messages_cz.py
@@ -4,5 +4,7 @@ from config.messages import Messages as GlobalMessages
 class MessagesCZ(GlobalMessages):
     bookmark_title = "Záložka na serveru `{server}`"
     bookmark_created = "Záložka `{title_name}` vytvořena"
+    bookmark_note = "Poznámka"
     upload_limit = "Zpráva obsahuje přílohu přesahující upload limit, doporučuji si tuto přílohu stáhnout. V připadě smazání původní zprávy nebude příloha dostupná."
     original_message = "Původní zpráva"
+    lottie_sticker = "Pokusil/a jsi se uložit výchozí Discord sticker. Tato API funkce nefunguje dobře a proto raději neposílám nic. Ulož si ji prosím jiným způsobem, pokud ji potřebuješ. <:KKomrade:484470873001164817>"
