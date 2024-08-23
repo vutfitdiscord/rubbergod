54ab6dd8110c59ba6209f39a2648c583a4d60da3
Peter Dragun <peter.dragun@espressif.com>
2024-08-23 20:47:09 +0200

    refactor: add __all__ section to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,10 @@
 from . import (
-    colors,  # noqa: F401
-    constants,  # noqa: F401
-    converters,  # noqa: F401
-    embed,  # noqa: F401
-    general,  # noqa: F401
-    user,  # noqa: F401
+    colors,
+    constants,
+    converters,
+    embed,
+    general,
+    user,
 )
+
+__all__ = ["colors", "constants", "converters", "embed", "general", "user"]
