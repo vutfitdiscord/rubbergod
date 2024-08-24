3d4bdf685261c7c83a463fe31c2d5a8aa1c61b5f
Solumath <solumath99@gmail.com>
2024-08-24 21:57:31 +0200

    absolvent: remove requests, use global session

--- cogs/absolvent/cog.py
+++ cogs/absolvent/cog.py
@@ -2,22 +2,25 @@
 Cog for diploma verification. When successful, the user is given Survivor(Bc.) or King(Ing.) role.
 """
 
+import asyncio
 import re
 import unicodedata
 
+import aiohttp
 import disnake
-import requests
 from disnake.ext import commands
 from lxml import etree
 
 from cogs.base import Base
 from database.verification import ValidPersonDB
+from permissions.custom_errors import ApiError
+from rubbergod import Rubbergod
 
 from .messages_cz import MessagesCZ
 
 
 class Absolvent(Base, commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: Rubbergod):
         super().__init__()
         self.bot = bot
 
@@ -45,11 +48,11 @@ class Absolvent(Base, commands.Cog):
         :param thesis_web_id: ID from URL http://hdl.handle.net/11012/<num>
             can be discovered via http://hdl.handle.net/11012/19121
         """
-        await inter.response.defer(with_message=True, ephemeral=True)
+        await inter.response.defer(ephemeral=True)
 
         author = await self.base_guild.fetch_member(inter.author.id)
         if not author:
-            # check whether the user is on server
+            # user is not on the server
             await inter.edit_original_response(MessagesCZ.not_on_server(user=inter.author.id))
             return
 
@@ -99,9 +102,14 @@ class Absolvent(Base, commands.Cog):
         thesis_url = f"http://hdl.handle.net/11012/{thesis_web_id}?locale-attribute=cs"
 
         # download the page
-        result_thesis = requests.get(thesis_url)
+        try:
+            async with self.bot.rubbergod_session.get(thesis_url) as response:
+                result_thesis = await response.text()
+        except (aiohttp.ClientConnectorError, asyncio.TimeoutError) as error:
+            raise ApiError(str(error))
+
         # parse it using lxml
-        xDoc_thesis = etree.fromstring(result_thesis.text, htmlparser)
+        xDoc_thesis = etree.fromstring(result_thesis, htmlparser)
         not_found = "".join(xDoc_thesis.xpath("/html/body/p[4]/text()"))
         breadcrumb_xpath = (
             "/html/body/ds-app/ds-themed-root/ds-root/div/div/main"
@@ -144,13 +152,17 @@ class Absolvent(Base, commands.Cog):
         diplom_url2 = "https://www.vut.cz/overeni-diplomu?aid_redir=1"
         diplom_url3 = "https://www.vut.cz/overeni-diplomu"
         data = {}
-        session = requests.session()
 
-        # load initial cookies + download the HTML-security codes in hidden input fields
-        session.get(diplom_url1)
-        result2 = session.get(diplom_url2)
+        try:
+            # load initial cookies + download the HTML-security codes in hidden input fields
+            await self.bot.rubbergod_session.get(diplom_url1)
+            async with self.bot.rubbergod_session.get(diplom_url2) as response:
+                result2 = await response.text()
+        except (aiohttp.ClientConnectorError, asyncio.TimeoutError) as error:
+            raise ApiError(str(error))
+
         # parse it using lxml
-        xDoc2 = etree.fromstring(result2.text, htmlparser)
+        xDoc2 = etree.fromstring(result2, htmlparser)
         # get all hidden server-generated inputs
         inputs = xDoc2.xpath("//*[@id='over_studenta']/input[@type='hidden']")
         for input in inputs:
@@ -163,8 +175,9 @@ class Absolvent(Base, commands.Cog):
         data["data[form_el_overit]"] = "Ověřit"
 
         # send the POST to check the diploma validity
-        result3 = session.post(diplom_url3, data)
-        xDoc3 = etree.fromstring(result3.text, htmlparser)
+        async with self.bot.rubbergod_session.post(diplom_url3, data=data) as response:
+            result3 = await response.text()
+        xDoc3 = etree.fromstring(result3, htmlparser)
         # parse the success text (in czech)
         absolventText = "".join(
             xDoc3.xpath(
@@ -182,22 +195,24 @@ class Absolvent(Base, commands.Cog):
         # Add according roles
         original_msg = await self.append_response(inter, original_msg, MessagesCZ.status_roles)
 
-        guild = self.bot.get_guild(self.config.guild_id)
-        role = None
+        absolvent_role = None
         if degree == "Bc.":
-            role = disnake.utils.get(guild.roles, id=self.config.bc_role_id)
-        if degree == "Ing.":
-            role = disnake.utils.get(guild.roles, id=self.config.ing_role_id)
-        if role:
-            member = guild.get_member(inter.author.id)
-            for drop_role in member.roles:
-                if "Dropout" in drop_role.name:
-                    await member.remove_roles(drop_role, reason="Diploma verification")
-            await member.add_roles(role)
+            absolvent_role = disnake.utils.get(self.base_guild.roles, id=self.config.bc_role_id)
+        elif degree == "Ing.":
+            absolvent_role = disnake.utils.get(self.base_guild.roles, id=self.config.ing_role_id)
+        if absolvent_role:
+            for role in author.roles:
+                if "Dropout" in role.name:
+                    await author.remove_roles(role, reason="Diploma verification")
+                    break
+            await author.add_roles(absolvent_role)
             await self.append_response(inter, original_msg, MessagesCZ.diplom_success)
 
     async def append_response(self, inter: disnake.ApplicationCommandInteraction, old: str, new: str) -> str:
         """Append new string to the original message and send"""
+        if old:
+            # remove loading emoji, add checkmark
+            old = old.rsplit("<", 1)[0] + "✅"
         msg = f"{old}\n{new}"
         await inter.edit_original_response(msg)
         return msg
