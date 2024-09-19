8282c88f449e54b386550e7f0b084c639df067e9
Lukas Tesar <lukastesar03@gmail.com>
2024-09-19 11:58:44 +0200

    resolve requested changes

--- features/verification.py
+++ features/verification.py
@@ -322,5 +322,5 @@ class Verification(BaseFeature):
 
         verify = disnake.utils.get(guild.roles, name="Verify")
         zajemce = disnake.utils.get(guild.roles, name="ZajemceoStudium")
-        newbie = member.guild.get_role(config.newbie_role)
+        newbie = disnake.utils.get(id=config.newbie_role)
         await member.remove_roles(host, verify, zajemce, newbie, reason="Verify user")
