7a690ebe086653b14b4d3392501692a5bc523cef
Solumath <solumath99@gmail.com>
2024-10-15 15:29:57 +0200

    changes to template

--- utils/checks.py
+++ utils/checks.py
@@ -2,6 +2,8 @@
 Contains functions to check permissions of users, channels and other.
 """
 
+from typing import Callable
+
 import disnake
 from disnake.ext import commands
 
@@ -16,149 +18,67 @@ PRIVILEGED_ROLES = [config.mod_role, config.submod_role, config.helper_role]
 
 
 class PermissionsCheck:
-    """Class containing methods to check permissions"""
+    """Class containing methods to check permissions
+
+    Methods using `check_template` are used as decorators for commands or as normal functions.
+    """
 
     @classmethod
     def is_bot_admin(
         cls,
         ctx: commands.Context | disnake.ApplicationCommandInteraction = None,
         raise_exception: bool = True,
-    ) -> bool:
-        """
-        Check if the user is a bot admin. Works as a function or a command decorator.
-
-        When `ctx` is passed, it works as a function.
-        When `ctx` is not passed, it acts as a decorator for a command.
-
-        Returns
-        -------
-        bool
-            True if user is an admin, False otherwise.
-        Exception
-            Throws `PermissionError` if `raise_exception=True` and user doesn't have permissions.
-        """
-
-        def predicate(ctx: commands.Context | disnake.ApplicationCommandInteraction) -> bool:
-            if ctx.author.id in config.admin_ids:
-                return True
-            if raise_exception:
-                raise PermissionError(message=Messages.bot_admin_only)
-            return False
-
-        if ctx:
-            # If ctx is passed, return the result of the predicate
-            return predicate(ctx)
-        else:
-            # If ctx is not passed, act as a decorator for command
-            return commands.check(predicate)
+    ):
+        """Check if the user is a bot admin."""
+        return cls.check_template(
+            ctx,
+            lambda ctx: ctx.author.id in config.admin_ids,
+            raise_exception,
+            PermissionError(Messages.bot_admin_only),
+        )
 
     @classmethod
     def is_mod_plus(
         cls,
         ctx: commands.Context | disnake.ApplicationCommandInteraction = None,
         raise_exception: bool = True,
-    ) -> bool:
-        """Check if user has permission for command mod or above.
-
-        When `ctx` is passed, it works as a function.
-        When `ctx` is not passed, it acts as a decorator for a command.
-
-        Returns
-        -------
-        bool
-            True if user is an admin, False otherwise.
-        Exception
-            Throws `PermissionError` if `raise_exception=True` and user doesn't have permissions.
-        """
-
-        def predicate(ctx: commands.Context | disnake.ApplicationCommandInteraction) -> bool:
-            if cls.is_bot_admin(ctx):
-                return True
-            if cls.role_check(ctx, PRIVILEGED_ROLES[:-2]):
-                return True
-
-            if raise_exception:
-                raise PermissionError(message=Messages.mod_plus_only)
-            return False
-
-        if ctx:
-            # If ctx is passed, return the result of the predicate
-            return predicate(ctx)
-        else:
-            # If ctx is not passed, act as a decorator for command
-            return commands.check(predicate)
+    ):
+        """Check if user has permission for command mod or above."""
+        return cls.check_template(
+            ctx,
+            lambda ctx: (cls.is_bot_admin(ctx) or cls.role_check(ctx, PRIVILEGED_ROLES[:-2])),
+            raise_exception,
+            PermissionError(Messages.mod_plus_only),
+        )
 
     @classmethod
     def is_submod_plus(
         cls,
         ctx: commands.Context | disnake.ApplicationCommandInteraction = None,
         raise_exception: bool = True,
-    ) -> bool:
-        """Check if user has permission for command submod or above.
+    ):
+        """Check if user has permission for command submod or above."""
 
-        When `ctx` is passed, it works as a function.
-        When `ctx` is not passed, it acts as a decorator for a command.
-
-        Returns
-        -------
-        bool
-            True if user is an admin, False otherwise.
-        Exception
-            Throws `PermissionError` if `raise_exception=True` and user doesn't have permissions.
-        """
-
-        def predicate(ctx: commands.Context | disnake.ApplicationCommandInteraction) -> bool:
-            if cls.is_bot_admin(ctx):
-                return True
-            if cls.role_check(ctx, PRIVILEGED_ROLES[:-1]):
-                return True
-
-            if raise_exception:
-                raise PermissionError(message=Messages.submod_plus_only)
-            return False
-
-        if ctx:
-            # If ctx is passed, return the result of the predicate
-            return predicate(ctx)
-        else:
-            # If ctx is not passed, act as a decorator for command
-            return commands.check(predicate)
+        return cls.check_template(
+            ctx,
+            lambda ctx: cls.is_bot_admin(ctx) or cls.role_check(ctx, PRIVILEGED_ROLES[:-1]),
+            raise_exception,
+            PermissionError(Messages.submod_plus_only),
+        )
 
     @classmethod
     def is_helper_plus(
         cls,
         ctx: commands.Context | disnake.ApplicationCommandInteraction = None,
         raise_exception: bool = True,
-    ) -> bool:
-        """Check if user has permission for command helper or above.
-
-        When `ctx` is passed, it works as a function.
-        When `ctx` is not passed, it acts as a decorator for a command.
-
-        Returns
-        -------
-        bool
-            True if user is an admin, False otherwise.
-        Exception
-            Throws `PermissionError` if `raise_exception=True` and user doesn't have permissions.
-        """
-
-        def predicate(ctx: commands.Context | disnake.ApplicationCommandInteraction) -> bool:
-            if cls.is_bot_admin(ctx):
-                return True
-            if cls.role_check(ctx, PRIVILEGED_ROLES):
-                return True
-
-            if raise_exception:
-                raise PermissionError(message=Messages.helper_plus_only)
-            return False
-
-        if ctx:
-            # If ctx is passed, return the result of the predicate
-            return predicate(ctx)
-        else:
-            # If ctx is not passed, act as a decorator for command
-            return commands.check(predicate)
+    ):
+        """Check if user has permission for command helper or above."""
+        return cls.check_template(
+            ctx,
+            lambda ctx: cls.is_bot_admin(ctx) or cls.role_check(ctx, PRIVILEGED_ROLES),
+            raise_exception,
+            PermissionError(Messages.helper_plus_only),
+        )
 
     @classmethod
     def role_check(
@@ -184,46 +104,28 @@ class PermissionsCheck:
         cls,
         ctx: commands.Context | disnake.ApplicationCommandInteraction = None,
         raise_exception: bool = True,
-    ) -> bool:
+    ):
         """Check if the command is invoked in modroom"""
-
-        def predicate(ctx: commands.Context | disnake.ApplicationCommandInteraction) -> bool:
-            if ctx.channel.id == config.mod_room:
-                return True
-
-            if raise_exception:
-                raise InvalidRoomError(Messages.specific_room_only(room=config.mod_room))
-            return False
-
-        if ctx:
-            # If ctx is passed, return the result of the predicate
-            return predicate(ctx)
-        else:
-            # If ctx is not passed, act as a decorator for command
-            return commands.check(predicate)
+        return cls.check_template(
+            ctx,
+            lambda ctx: ctx.channel.id == config.mod_room,
+            raise_exception,
+            InvalidRoomError(Messages.specific_room_only(room=config.mod_room)),
+        )
 
     @classmethod
     def is_in_voteroom(
         cls,
         ctx: commands.Context | disnake.ApplicationCommandInteraction = None,
         raise_exception: bool = True,
-    ) -> bool:
+    ):
         """Check if the command is invoked in voteroom"""
-
-        def predicate(ctx: commands.Context | disnake.ApplicationCommandInteraction) -> bool:
-            if ctx.channel.id == config.vote_room:
-                return True
-
-            if raise_exception:
-                raise InvalidRoomError(Messages.specific_room_only(room=config.vote_room))
-            return False
-
-        if ctx:
-            # If ctx is passed, return the result of the predicate
-            return predicate(ctx)
-        else:
-            # If ctx is not passed, act as a decorator for command
-            return commands.check(predicate)
+        return cls.check_template(
+            ctx,
+            lambda ctx: ctx.channel.id == config.vote_room,
+            raise_exception,
+            InvalidRoomError(Messages.specific_room_only(room=config.vote_room)),
+        )
 
     @classmethod
     def is_botroom(cls, inter: disnake.ApplicationCommandInteraction) -> bool:
@@ -249,3 +151,38 @@ class PermissionsCheck:
             return False
 
         return True
+
+    @classmethod
+    def check_template(
+        cls,
+        ctx: commands.Context | disnake.ApplicationCommandInteraction,
+        check: Callable,
+        raise_exception: bool,
+        exception: Exception,
+    ) -> bool:
+        """Check if the command is invoked in voteroom
+
+        When `ctx` is passed, it works as a function.
+        When `ctx` is not passed, it acts as a decorator for a command.
+
+        Returns
+        -------
+        bool
+            based on the passed check
+        Exception
+            Throws `Exception` if `raise_exception=True` and `check=False`
+        """
+
+        def predicate(ctx: commands.Context | disnake.ApplicationCommandInteraction) -> bool:
+            if check(ctx):
+                return True
+            if raise_exception:
+                raise exception
+            return False
+
+        if ctx:
+            # If ctx is passed, return the result of the predicate
+            return predicate(ctx)
+        else:
+            # If ctx is not passed, act as a decorator for command
+            return commands.check(predicate)
