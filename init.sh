83cfa468172fea6c690f9189447bf3b3fdb90a06
Solumath <solumath99@gmail.com>
2025-01-29 15:47:16 +0100

    support multi-server emoji download

--- init.sh
+++ init.sh
@@ -7,6 +7,6 @@ DEFAULT_GID=$(id -g)
 
 # Create logs folder with permissions
 echo "Updating logs folder permissions"
-mkdir -p logs
-sudo chmod -R 777 logs
-sudo chown -R $DEFAULT_UID:$DEFAULT_GID logs/
+mkdir -p logs guilds
+sudo chmod -R 777 logs guilds
+sudo chown -R $DEFAULT_UID:$DEFAULT_GID logs/ guilds/
